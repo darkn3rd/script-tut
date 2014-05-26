5c3e630b4598e263d0087e51c18b7f0ff663f0aa
Joaquin Menchaca <joaquinmenchaca@Joaquins-MacBook-Pro.local>
2014-05-26 01:50:29 -0700

    POSIX Shell - function update

--- shell/l02.function.sh
+++ shell/l02.function.sh
@@ -2,15 +2,15 @@
 
 # create function (subroutine) 
 add() {
-  sum=0
-  for num in "$@"; do
-    sum=$(( $sum + $num ))
+  sum=0                     # initialize starting sum value
+  for num in "$@"; do       # iterate through parameters
+    sum=$(( $sum + $num ))  # add num to sum
   done
  
-  result=$sum
+  result=$sum               # outer scop result set to $sum
 }
 
-# call function in subshell, save result 
+# call function and use side-effect 
 add 5 2 4 3 6
-
+# output result
 echo "The result of summation is: $result"
