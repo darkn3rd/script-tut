0a63efc7b50646262022034384f2b614fc84538d
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-03 09:59:19 -0700

    python date

--- python/i00.subroutine.py
+++ python/i00.subroutine.py
@@ -2,7 +2,7 @@
 import time
 # create method (subroutine)
 def show_date():
-   print "Today is %s." % time.strftime("%b %d, %Y")
+   print "Today is %s." % time.strftime("%B %d, %Y")
 
 # call the method (subroutine)
 show_date()
