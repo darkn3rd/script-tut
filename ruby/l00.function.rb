11c7f195c1e7b84331c9976ba9c3d2f5532b67f5
지훈 <sauvecali@yahoo.com>
2014-06-02 23:30:28 -0700

    ruby functions

--- ruby/l00.function.rb
+++ ruby/l00.function.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/ruby
 
 # create method (function)
-def add (*numbers)
+def addNums (*numbers)
    sum = 0                          # initialize starting sum value
    numbers.each {|num| sum += num } # iterate and sum up nums
    sum                              # return value
 end
 
 # call the method (function)
-result = add 5, 2, 4, 3, 6
+result = addNums 5, 2, 4, 3, 6
 
 # output results
 puts "The result of summation is: #{result}"
