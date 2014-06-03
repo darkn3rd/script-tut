7de68a622a44d7aee5b17dacb072e3b6732afe22
darkn3rd <bufus@bufus.co.be>
2014-06-03 10:27:21 -0700

    pshell param

--- powershell/k00.parameters.ps1
+++ powershell/k00.parameters.ps1
@@ -1,1 +1,11 @@
+# create function (subroutine)
+Function Celsius ($fahrenheit)
+{
+   $temperature = ($fahrenheit - 32.0) * 5 / 9 
+   $temperature = "{0:N1}" -f $temperature
+   Write-Host "The Celsius temperature is ${temperature} degrees."
+}
 
+$temperature = 73
+# call the function (subroutine)
+celsius $temperature 
