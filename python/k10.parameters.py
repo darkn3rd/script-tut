176dacc4314d5c4df226c5f7f1822a3fe225763d
Joaquin Menchaca <joaquinmenchaca@Joaquins-MacBook-Pro.local>
2014-06-02 23:25:10 -0700

    python params

--- python/k10.parameters.py
+++ python/k10.parameters.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # create method (subroutine)
-def add (*numbers):
+def addNums (*numbers):
    sum = 0                            # initialize starting sum value
    for num in numbers:                # iterate
       sum += num                      # sum up nums
@@ -9,4 +9,4 @@ def add (*numbers):
 
 
 # call the method (subroutine)
-add(5, 2, 4, 3, 6)
+addNums(5, 2, 4, 3, 6)
