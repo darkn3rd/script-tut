66623e7a7ca04614cf463574c9c1af6124b03820
darkn3rd <bufus@bufus.co.be>
2014-06-03 12:50:46 -0700

    pshell args

--- powershell/j11.arguments.ps1
+++ powershell/j11.arguments.ps1
@@ -1,5 +1,5 @@
 "The arguments passed are:"
 foreach($count in 1..$args.Count) { 
-  $arg = $args[$count-1]
-  " item ${count}: ${arg}" 
+  $arg = $args[$count-1]             # retreived exact item
+  " item ${count}: ${arg}"           # output results
 }
