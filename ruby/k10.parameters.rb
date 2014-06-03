f60be4c0328c87e48a27297478e43dddf490a97b
지훈 <sauvecali@yahoo.com>
2014-06-02 23:32:07 -0700

    ruby params

--- ruby/k10.parameters.rb
+++ ruby/k10.parameters.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/ruby
 
 # create method (subroutine)
-def addNum (*numbers)
+def addNums (*numbers)
    sum = 0                          # initialize starting sum value
    numbers.each {|num| sum += num } # iterate and sum up nums
    puts "The summation is: #{sum}"  # output results
 end
 
 # call the method (subroutine)
-addNum 5, 2, 4, 3, 6
+addNums 5, 2, 4, 3, 6
