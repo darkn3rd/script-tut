18ac4c0480f82d9a66e1b7d24864fb42f747c5fb
darkn3rd <bufus@bufus.co.be>
2014-06-03 12:21:11 -0700

    pshell function

--- powershell/l00.function.ps1
+++ powershell/l00.function.ps1
@@ -1,1 +1,11 @@
+# create function (subroutine)
+Function Add-Nums ($nums)
+{
+   foreach($num in $nums) { $sum += $num }  # interate and sum up nums
+   $sum
+}
 
+# call the function (subroutine)
+$result = Add-Nums 5, 2, 4, 3, 6
+# output results
+"The result of summation is: $result."
--- powershell/l10.function.ps1
+++ powershell/l10.function.ps1
@@ -1,1 +1,9 @@
 
+# create function (subroutine)
+Function Capitalize ($string) { $string.ToUpper() }
+
+# call the function (subroutine)
+$result = Capitalize "ibm"
+
+# output results
+"The resulting string is: $result."
