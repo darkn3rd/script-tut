0c57df42a13bb2adfcc52e7ac47a694831eae088
Joaquin Menchaca <joaquinmenchaca@Joaquins-MacBook-Pro.local>
2014-06-02 23:23:58 -0700

    python params

--- python/k00.parameters.py
+++ python/k00.parameters.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 
 # create method (subroutine)
-def add (*numbers):
-   sum = 0                            # initialize starting sum value
-   for num in numbers:                # iterate
-      sum += num                      # sum up nums
-   print "The summation is: %d" % sum # output results
+def celsius (fahrenheit):
+   temperature = (fahrenheit - 32.0) * 5 / 9
+   print "The Celsius temperature is %.1f degrees." % temperature
 
-
+temperature = 73
 # call the method (subroutine)
-add(5, 2, 4, 3, 6)
+celsius(temperature)
