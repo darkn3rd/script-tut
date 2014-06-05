b6514168639ddbddc13d8e64f0fdd4288f5904a8
darkn3rd <bufus@bufus.co.be>
2014-06-05 16:48:14 -0700

    jscript RegExp

--- jscript/e20.branch.js
+++ jscript/e20.branch.js
@@ -1,21 +1,14 @@
-' prompt and read one character
-wscript.stdout.write "Input a character: "
-keypress = wscript.stdin.read(1)
+// prompt and read one character
+WScript.stdout.write("Input a character: ");
+keypress = WScript.stdin.read(1);
 
-' find match and print result
-if match("[a-z]", keypress) then
-    wscript.echo "Lowercase letter"
-elseif match("[A-Z]", keypress) then
-    wscript.echo "Uppercase letter"
-elseif (match("[0-9]", keypress)) then
-    wscript.echo "Digit"
-else
-    wscript.echo "Punctuation, whitespace, or other"
-end if
-
-' match() - returns true if pattern found in string
-function match(pattern, string)
-  set re = new regexp
-  re.pattern = pattern
-  match = re.test(string)
-end function
+// find RegExp and print result
+if (RegExp("[a-z]").test(keypress)) {
+    WScript.Echo("Lowercase letter");
+} else if (RegExp("[A-Z]").test(keypress)) {
+    WScript.Echo("Uppercase letter");
+} else if (RegExp("[0-9]").test(keypress)) {
+    WScript.Echo("Digit");
+} else {
+    WScript.Echo("Punctuation, whitespace, or other");
+}
