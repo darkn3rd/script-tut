c4364acc0064add667fe2b27440cdcd8a942ca47
darkn3rd <suavecali@yahoo.com>
2014-06-05 21:31:04 -0700

    jscript branch

--- jscript/e20.branch.js
+++ jscript/e20.branch.js
@@ -3,12 +3,11 @@ WScript.stdout.write("Input a character: ");
 keypress = WScript.stdin.read(1);
 
 // find RegExp and print result
-if (RegExp("[a-z]").test(keypress)) {
+if (RegExp("[a-z]").test(keypress)) 
     WScript.Echo("Lowercase letter");
-} else if (RegExp("[A-Z]").test(keypress)) {
-    WScript.Echo("Uppercase letter");
-} else if (RegExp("[0-9]").test(keypress)) {
+else if (RegExp("[A-Z]").test(keypress)) 
+   WScript.Echo("Uppercase letter");
+else if (RegExp("[0-9]").test(keypress)) 
     WScript.Echo("Digit");
-} else {
+else 
     WScript.Echo("Punctuation, whitespace, or other");
-}
