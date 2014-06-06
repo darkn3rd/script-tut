a1806db8fa77884c8e09213389338e6fcae80e1b
darkn3rd <suavecali@yahoo.com>
2014-06-05 21:32:30 -0700

    jscript branch

--- jscript/e00.branch.js
+++ jscript/e00.branch.js
@@ -1,10 +1,9 @@
 WScript.stdout.write("Input a number: ");
 number = WScript.stdin.readline();
 
-if (number > 0) {                            // automatic coercion to int
+if (number > 0)                             // automatic coercion to int
    WScript.Echo("Number is greater than 0");
-} else if (number < 0) { 
+else if (number < 0)  
    WScript.Echo("Number is less than 0");
-} else {
+else 
    WScript.Echo("Number is 0");
-}
