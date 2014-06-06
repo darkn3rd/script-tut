80c0aabbf0832127e8e8b32944ff326a674b78ef
darkn3rd <suavecali@yahoo.com>
2014-06-05 22:18:21 -0700

    jscript array

---
+++ jscript/h10.associative.js
@@ -0,0 +1,18 @@
+// populate initial associative Array
+var ages = {"bob":34, "ed":58, "steve":32, "ralph":23};
+
+// append more items into associative Array
+ages = merge(ages, {"deb":46, "kate":19});
+
+// iterate through dictionary, print key/value pairs
+WScript.Echo("The ages are: ")
+for(var name in ages)  
+    WScript.Echo(" ages[" + name + "]=" + ages[name]);
+
+// merge() - merges two arrays 
+function merge(array1, array2) 
+{
+  for (key in array1) 
+    array2[key] = array1[key];
+  return array2;
+}
