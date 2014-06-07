352aeb34a02ffe2cca3a9e41e2cb50d1af3d8711
darkn3rd <suavecali@yahoo.com>
2014-06-06 18:20:55 -0700

    jscript args

--- jscript/j20.arguments.js
+++ jscript/j20.arguments.js
@@ -1,6 +1,6 @@
-WScript.Echo "The arguments passed are (reverse order):"
-' iterative loop from # of args to 1
-For count = (Wscript.Arguments.Count) To 1 step -1
-  ' output count and corresponding argument item
-  WScript.Echo " item " & count & ": " & WScript.Arguments.Item(count-1)
-next
+WScript.Echo("The arguemnts passed are (reverse order):");
+
+// print count and current argument
+for(var count=WScript.Arguments.length; count > 0; count--) { 
+  WScript.Echo(" item " + (count) + ": " + WScript.Arguments.Item(count-1)); 
+}
