e401c74f9a61abe2c2b202fb9f854a99d70edb2b
智裕 <joaquin.it@gmail.com>
2014-06-07 12:17:39 -0700

    Update e00.branch.tcl

--- tcl/e00.branch.tcl
+++ tcl/e00.branch.tcl
@@ -1,1 +1,13 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+# prompt and get input
+puts -nonewline "Input a number: "
+flush stdout
+gets stdin number
+# evaluate input & print result
+if {$number > 0} {
+  puts "Number is greater than 0"
+} elseif {$number < 0} {
+  puts "Number is less than 0"
+} else {
+  puts "Number is 0"
+}
