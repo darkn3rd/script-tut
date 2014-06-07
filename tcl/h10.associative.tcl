9652cdecfd73486bd5a8110fdd63d62acf999b8e
智裕 <joaquin.it@gmail.com>
2014-06-07 12:31:07 -0700

    Update h10.associative.tcl

--- tcl/h10.associative.tcl
+++ tcl/h10.associative.tcl
@@ -1,1 +1,9 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+# populate initial array
+array set ages {bob 34 ed 58 steve 32 ralph 23}
+# append more items into array
+array set ages {deb 46 kate 19}
+# iterate through array, print key/value pairs
+foreach {name age} [array get ages] {
+  puts "ages\[$name\]=$age"
+}
