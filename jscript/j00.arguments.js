a113c84460c07d6aedf2ff771a96130f28bb382e
darkn3rd <suavecali@yahoo.com>
2014-06-06 18:02:44 -0700

    jscript args

--- jscript/j00.arguments.js
+++ jscript/j00.arguments.js
@@ -1,14 +1,22 @@
-' check for only 2 arguments
-If Wscript.Arguments.Count <> 2 Then
- ' get host environment
- ' Note: Assumes C:\Windows\SysWOW64\cscript.exe or something similar
- ScriptHostName = Split(WScript.FullName, "\")(3)  ' get 4th element after split
- ' print helpful instructions
- WScript.Echo "You need to enter two numbers: " & vbCrLf & vbCrLf & _
-              vbTab & ScriptHostName & " //NoLogo " & WScript.ScriptName & " num1 num2"
-Else
- ' print sum of the two integer arguments
- WScript.Echo "The sum of " & WScript.Arguments.Item(0) & _
-              " and " & WScript.Arguments.Item(1) & _
-              " is: " & (Int(WScript.Arguments.Item(0)) + WScript.Arguments.Item(1))
-End If
+// check for only two arguments
+if (WScript.Arguments.Count() != 2) {
+
+ // get host environment
+
+ // Note: Assumes C:\Windows\SysWOW64\cscript.exe or something similar
+ ScriptHostName = WScript.FullName.split("\\")[3]; // get 4th element after split
+
+ // print helpful instructions
+ WScript.Echo("You need to enter two numbers: \n\n\t" +
+              ScriptHostName + " //NoLogo " + WScript.ScriptName + " num1 num2");
+
+} else {
+
+ // print sum of the two integer arguments
+ WScript.Echo("The sum of " + WScript.Arguments.Item(0) + 
+              " and " + WScript.Arguments.Item(1) + " is: " + 
+              (parseInt(WScript.Arguments.Item(0)) + parseInt(WScript.Arguments.Item(1)))
+             );
+
+}
+
