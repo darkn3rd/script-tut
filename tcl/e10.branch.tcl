a145e4d0006ba0553be1dc045dfe3e72f91316e2
智裕 <joaquin.it@gmail.com>
2014-06-07 12:18:20 -0700

    Update e10.branch.tcl

--- tcl/e10.branch.tcl
+++ tcl/e10.branch.tcl
@@ -1,1 +1,12 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+# prompt and get input
+puts -nonewline "Input a character: "
+flush stdout
+set keypress [read stdin 1]
+# switch on the input (match pattern) and output result
+switch -regexp $keypress {
+  [a-z] { puts "Lowercase letter" }
+  [A-Z] { puts "Uppercase letter" }
+  [0-9] { puts "Digit" }
+  default { puts "Punctuation, whitespace, or other" }
+}
