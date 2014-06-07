68c7436781b3ac1ea355e9d8871be935c95e6fa9
智裕 <joaquin.it@gmail.com>
2014-06-07 12:15:05 -0700

    Update c00.arithmetic.tcl

--- tcl/c00.arithmetic.tcl
+++ tcl/c00.arithmetic.tcl
@@ -1,1 +1,9 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+# integer arithmetic
+set width 5; set length 6
+set area [expr $width * $length]
+puts "The area of a square(width=$width, length=$length) is $area"
+ 
+# bolean logic
+set result [expr True && False || True]
+puts "The statement (true AND false OR true) is $result"
