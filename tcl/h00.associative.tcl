2621332a9766a9653e5f54350925236e84aebf2c
智裕 <joaquin.it@gmail.com>
2014-06-07 12:29:19 -0700

    Update h00.associative.tcl

--- tcl/h00.associative.tcl
+++ tcl/h00.associative.tcl
@@ -1,1 +1,13 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+# insert one element at a time
+set ages(bob)   34
+set ages(ed)    58
+set ages(steve) 32
+set ages(ralph) 23
+set ages(deb)   46
+set ages(kate)  19
+# enumerate & print keys
+puts "Keys (names): [array names ages]"
+# enumerate & print values
+puts -nonewline "Values (set ages): "
+puts [lsearch -all -inline -regexp [split [array get ages]] {[0-9]+}]
