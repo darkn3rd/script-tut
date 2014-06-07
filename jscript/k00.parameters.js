29b17ebc0c854e7169a3ce1b451dbc85b48ab517
darkn3rd <suavecali@yahoo.com>
2014-06-06 19:56:32 -0700

    jscript params

--- jscript/k00.parameters.js
+++ jscript/k00.parameters.js
@@ -1,11 +1,11 @@
-' create subroutine
-Sub celsius(fahrenheit)
-  temperature = (fahrenheit - 32) * 5 / 9   ' calculate to internatioanl temp
-  temperature = FormatNumber(temperature,1) ' lower degree of significance
+// create subroutine
+function celsius(fahrenheit) {
+  temperature = (fahrenheit - 32) * 5 / 9; // calculate to internatioanl temp
+  temperature = temperature.toFixed(1);    // lower degree of significance
 
-  ' output results
-  WScript.echo "The Celsius temperature is " & temperature & " degrees."
-End Sub
+  // output results
+  WScript.echo("The Celsius temperature is " + temperature + " degrees.");
+}
 
-temperature = 73     ' set temp in fahrenheit
-celsius temperature  ' call subroutine
+temperature = 73;     // set temp in fahrenheit
+celsius(temperature); // call subroutine
