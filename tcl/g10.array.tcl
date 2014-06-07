774157b736a3645ec4f1891312546c27586f3bef
智裕 <joaquin.it@gmail.com>
2014-06-07 12:26:54 -0700

    Update g10.array.tcl

--- tcl/g10.array.tcl
+++ tcl/g10.array.tcl
@@ -1,1 +1,8 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+# create populated list
+set nicknames [list bob ed steve ralph joe deb kate] 
+# enumerate list one element at a time
+puts "The names are: "
+foreach name $nicknames {
+    puts  " $name"
+}
