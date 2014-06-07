7a75a4578857b3629c8ffff5a1a0ab72d0e74a34
智裕 <joaquin.it@gmail.com>
2014-06-07 12:23:19 -0700

    Update f20.loop.tcl

--- tcl/f20.loop.tcl
+++ tcl/f20.loop.tcl
@@ -1,1 +1,9 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+# foreach construction /w exec
+foreach item [exec "ls"] {
+   if {[file isdirectory $item]} {
+       puts "$item is a directory."
+   } else {
+       puts "$item is not a directory."
+   }
+}
