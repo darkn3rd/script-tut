2b1625106cb27399f55cc138e4005ddebbcc1ba0
智裕 <joaquin.it@gmail.com>
2014-06-06 18:04:45 -0700

    Update j00.arguments.js

--- jscript/j00.arguments.js
+++ jscript/j00.arguments.js
@@ -1,8 +1,6 @@
 // check for only two arguments
 if (WScript.Arguments.Count() != 2) {
-
  // get host environment
-
  // Note: Assumes C:\Windows\SysWOW64\cscript.exe or something similar
  ScriptHostName = WScript.FullName.split("\\")[3]; // get 4th element after split
 
@@ -11,12 +9,10 @@ if (WScript.Arguments.Count() != 2) {
               ScriptHostName + " //NoLogo " + WScript.ScriptName + " num1 num2");
 
 } else {
-
  // print sum of the two integer arguments
  WScript.Echo("The sum of " + WScript.Arguments.Item(0) + 
               " and " + WScript.Arguments.Item(1) + " is: " + 
               (parseInt(WScript.Arguments.Item(0)) + parseInt(WScript.Arguments.Item(1)))
              );
-
 }
 
