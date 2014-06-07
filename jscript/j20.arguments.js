e43e0ba719136068a9bb96d078587dc1e3b3f8aa
智裕 <joaquin.it@gmail.com>
2014-06-06 18:22:24 -0700

    Update j20.arguments.js

--- jscript/j20.arguments.js
+++ jscript/j20.arguments.js
@@ -1,4 +1,4 @@
-WScript.Echo("The arguemnts passed are (reverse order):");
+WScript.Echo("The arguments passed are (reverse order):");
 
 // print count and current argument
 for(var count=WScript.Arguments.length; count > 0; count--) { 
