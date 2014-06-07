c60bc9ba67f6a5811f4c047b7b219ca639c84b3b
智裕 <joaquin.it@gmail.com>
2014-06-07 12:28:04 -0700

    Update g20.array.tcl

--- tcl/g20.array.tcl
+++ tcl/g20.array.tcl
@@ -1,1 +1,9 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+# populate full list
+set nicknames [list bob ed steve ralph joe deb kate]
+# enumerate through list elements by index
+puts "The names are: "
+for {set count 0} {$count < [llength $nicknames]} {incr count} {
+   puts -nonewline " nicknames\[$count\]="
+   puts [lindex $nicknames $count]
+}
