6ba1e53aadde4202cd7c5ff87d6b2d93759be0d0
智裕 <joaquin.it@gmail.com>
2014-06-07 12:34:00 -0700

    Update h11.associative.tcl

--- tcl/h11.associative.tcl
+++ tcl/h11.associative.tcl
@@ -1,1 +1,9 @@
-# git hub web editor is buggles...
+#!/usr/bin/tclsh
+# populate initial array
+array set ages {bob 34 ed 58 steve 32 ralph 23}
+# append more items into array
+array set ages {deb 46 kate 19}
+# iterate through array, print key/value pairs
+foreach name [array names ages] {
+  puts "ages\[$name\]=$ages($name)"
+}
