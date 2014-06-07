af80dd7d4dfac7ef65daedd94b81bd7933e9ab46
智裕 <joaquin.it@gmail.com>
2014-06-07 12:14:37 -0700

    Update b00.variables.tcl

--- tcl/b00.variables.tcl
+++ tcl/b00.variables.tcl
@@ -1,1 +1,8 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+set num    5
+set char   'a'
+set string "This is a string"
+ 
+puts "Number is $num"
+puts "Character is a $char"
+puts "String is \"$string\"."
