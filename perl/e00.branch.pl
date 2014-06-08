19caffd804ddf06ef883cc390ab16aa39b94bf3e
智裕 <joaquin.it@gmail.com>
2014-06-08 12:11:44 -0700

    Update e00.branch.pl

--- perl/e00.branch.pl
+++ perl/e00.branch.pl
@@ -1,1 +1,9 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+print "Input a number: "; my $Number=<>; chomp $Number;
+if ( $Number > 0 ) {
+    print "Number is greater than 0\n";
+} elsif ( $Number < 0 ) {
+    print "Number is less than 0\n";
+} else {
+    print "Number is 0\n";
+}
