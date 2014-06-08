7ff8f346f528017edf2a0e3ac6129ad1506f90f7
智裕 <joaquin.it@gmail.com>
2014-06-08 13:24:38 -0700

    Update e20.branch.bash

--- bash/e20.branch.bash
+++ bash/e20.branch.bash
@@ -1,1 +1,14 @@
-#!//bin/bash
+#!/bin/bash
+read -p "Input a character: " keypress # prompt & get input
+keypress=${keypress:0:1}               # substring for only 1st char
+ 
+# test if keypress matches pattern
+if [[ $keypress = [[:lower:]] ]]; then
+  echo "Lowercase letter"
+elif [[ $keypress = [[:upper:]] ]]; then
+  echo "Uppercase letter"
+elif [[ $keypress = [[:digit:]] ]]; then
+  echo "Digit"
+else
+  echo "Punctuation, whitepace, or other"
+fi
