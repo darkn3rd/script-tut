7972eddae820ee6f308fa0f75935cb13065c07ed
智裕 <joaquin.it@gmail.com>
2014-06-08 12:56:41 -0700

    Update e20.branch.sh

--- shell/e20.branch.sh
+++ shell/e20.branch.sh
@@ -2,6 +2,8 @@
 echo -n "Input a character: " ; read Keypress
  
 # if construction
+# Note that POSIX Shell does not support POSIX character class expressions
+#  As POSIX tr supports POSIX character class expression, this is workaround
 if [ -z $(echo $Keypress | tr -d "[:lower:]") ]; then
   echo "Lowercase letter"
 elif [ -z $(echo $Keypress | tr -d "[:upper:]") ]; then
