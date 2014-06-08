43901f008a973dcc090cdb836f551dee10a0f5ba
智裕 <joaquin.it@gmail.com>
2014-06-08 13:25:38 -0700

    Update f10.loop.bash

--- bash/f10.loop.bash
+++ bash/f10.loop.bash
@@ -1,1 +1,8 @@
-#!//bin/bash
+#!/bin/bash
+# loop until user exits using until/do...done construction
+until [[ "$answer" = "quit" ]]; do
+   read -p "Enter your name (quit to exit): " answer # prompt and get input
+   if [[ $answer != "quit" ]] ; then                 # echo if not exiting
+    echo Hello $answer
+   fi
+done
