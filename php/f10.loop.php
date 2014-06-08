09301d45f8bb1c4b6c10e72e94ee0e8f46be18c8
智裕 <joaquin.it@gmail.com>
2014-06-08 11:06:41 -0700

    Update f10.loop.php

--- php/f10.loop.php
+++ php/f10.loop.php
@@ -1,1 +1,11 @@
 #!/usr/bin/php
+<?php
+// do...while construction
+do {
+    echo "Enter your name (quit to Exit): ";
+    $answer = fgets(STDIN);
+    if ($answer != "quit\n") {
+        echo "Hello $answer";
+    }
+} while ($answer != "quit\n");
+?>
