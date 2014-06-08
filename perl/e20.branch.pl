e37e5e2ab6123f9305964068b02909cbe3de080f
智裕 <joaquin.it@gmail.com>
2014-06-08 12:14:03 -0700

    Update e20.branch.pl

--- perl/e20.branch.pl
+++ perl/e20.branch.pl
@@ -1,1 +1,12 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+print "Input a character: "; $keypress=getc(STDIN);
+  
+if ($keypress =~ /[[:lower:]]/) {
+    print "Lowercase letter\n";
+} elsif ($keypress =~ /[[:upper:]]/) {
+    print "Uppercase letter\n";
+} elsif ($keypress =~ /[[:digit:]]/) {
+    print "digit\n";
+} else {
+    print "Punctuation, whitespace, or other\n";
+}
