87a1d821afd540b6ab47a851fed36eb428e5a791
智裕 <joaquin.it@gmail.com>
2014-06-08 11:14:24 -0700

    Create b10.variables.php

---
+++ php/b10.variables.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php
+$num = 5;
+$chr = 'a';
+$str = "This is a string";
+ 
+echo "Number is " . $num . ".\n";
+echo "Character is '" . $chr" . '.\n";
+echo "String is \"" . $str . "\".\n";
+?>
