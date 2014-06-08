97095dcbf309cacd14ed2208fdc7674e2c76d34c
智裕 <joaquin.it@gmail.com>
2014-06-08 13:22:48 -0700

    Update c20.arithmetic.bash

--- bash/c20.arithmetic.bash
+++ bash/c20.arithmetic.bash
@@ -1,1 +1,5 @@
-#!//bin/bash
+#!/bin/bash
+declare -r PI=3.14159265359           # set approximation of π
+radius=3                              # set radius
+area=$(echo "$PI * $radius ^ 2" | bc) # calculate area, no float in bash
+echo "The area of a circle is $area"  # output result
