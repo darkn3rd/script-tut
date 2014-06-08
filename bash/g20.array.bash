87d27465ca8e4524f5f6e8d3d60da690d198bf5a
智裕 <joaquin.it@gmail.com>
2014-06-08 13:26:55 -0700

    Update g20.array.bash

--- bash/g20.array.bash
+++ bash/g20.array.bash
@@ -1,1 +1,8 @@
-#!//bin/bash
+#!/bin/bash
+ 
+declare -a nicknames=(bob ed steve ralph joe deb kate)
+ 
+echo "The names are: "
+for (( count=0; count < ${#nicknames[*]}; count++ )); do
+  echo " nicknames[$count]=${nicknames[$count]}"
+done
