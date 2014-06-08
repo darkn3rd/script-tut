645ce4db49a9062fa4577b0632b85b5afbac447b
智裕 <joaquin.it@gmail.com>
2014-06-08 11:11:39 -0700

    Update h00.associative.php

--- php/h00.associative.php
+++ php/h00.associative.php
@@ -1,1 +1,12 @@
 #!/usr/bin/php
+<?php
+$ages[bob]=34;
+$ages[ed]=58;
+$ages[steve]=32;
+$ages[ralph]=23;
+$ages[deb]=46;
+$ages[kate]=19;
+ 
+echo "Keys (names): ", join(" ", (array_keys($ages))), "\n";
+echo "Values (ages): ", join(" ", (array_values($ages))), "\n";
+?>
