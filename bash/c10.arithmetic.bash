ac2ccf889fd431a9c1564cecf52a4ad6a4428f50
智裕 <joaquin.it@gmail.com>
2014-06-08 13:21:47 -0700

    Update c10.arithmetic.bash

--- bash/c10.arithmetic.bash
+++ bash/c10.arithmetic.bash
@@ -1,1 +1,10 @@
-#!//bin/bash
+#!/bin/ksh
+# integer arithmatic
+width=5; length=6
+area=$(($length * $width))
+echo "The area of a square (width=$width, length=$length) is $area"
+ 
+# boolean logic
+true=1; false=0
+result=$(($true && $false || $true))
+echo "The statement (true AND false OR true) is $result"
