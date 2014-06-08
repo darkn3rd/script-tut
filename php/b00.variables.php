0f3c5af11defc0c12fd18b678ad3ff445cafb0ac
智裕 <joaquin.it@gmail.com>
2014-06-08 11:16:04 -0700

    Update b00.variables.php

--- php/b00.variables.php
+++ php/b00.variables.php
@@ -1,9 +1,11 @@
 #!/usr/bin/php
 <?php
+# declare variables
 $num = 5;
 $chr = 'a';
 $str = "This is a string";
  
+# output results using string interpolation 
 echo "Number is $num.\n";
 echo "Character is '$chr'.\n";
 echo "String is \"$str\".\n";
