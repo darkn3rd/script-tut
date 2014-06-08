20c2ff6f04a9791c0048368f9b0c705040880000
智裕 <joaquin.it@gmail.com>
2014-06-08 12:27:50 -0700

    Update g00.array.pl

--- perl/g00.array.pl
+++ perl/g00.array.pl
@@ -1,1 +1,14 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+my @nicknames;          # declare list for good form
+  
+# insert one by one into list
+$nicknames[0]="bob";
+$nicknames[1]="ed";
+$nicknames[2]="steve";
+$nicknames[3]="ralph";
+$nicknames[4]="joe";
+$nicknames[5]="deb";
+$nicknames[6]="kate";
+  
+print "The total names are: ", scalar(@nicknames),"\n";
+print "The names are: @nicknames \n";
