50a6164bad2e2213426c0dd75dce675038e7f3b7
智裕 <joaquin.it@gmail.com>
2014-06-08 13:24:04 -0700

    Update e00.branch.bash

--- bash/e00.branch.bash
+++ bash/e00.branch.bash
@@ -1,1 +1,11 @@
-#!//bin/bash
+#!/bin/bash
+# prompt user and get input
+read -p "Input a number: " number
+# test input and output result
+if (( $number > 0 )); then
+  echo Number is greater than 0
+elif (( $number < 0 )); then
+  echo Number is less than 0
+else
+  echo Number is 0
+fi
