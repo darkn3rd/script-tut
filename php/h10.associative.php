7dc12776f116ba9ae3a02bddc53f112a2a84180a
智裕 <joaquin.it@gmail.com>
2014-06-08 11:12:13 -0700

    Update h10.associative.php

--- php/h10.associative.php
+++ php/h10.associative.php
@@ -1,1 +1,12 @@
 #!/usr/bin/php
+<?php
+// initialize array with key/value pairs
+$ages = array("bob"=>34, "ed"=>58, "steve"=>32, "ralph"=>23);
+// append another set of key/value pairs into array
+$ages = array_merge($ages, array("deb"=>46, "kate"=>19));
+ 
+print "The ages are: \n";
+foreach (array_keys($ages) as $name) {
+    echo " ages[$name]=$ages[$name]\n";
+}
+?>
