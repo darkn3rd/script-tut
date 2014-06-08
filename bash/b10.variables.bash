c7eee4413324754e683debf7e4437929ad7c2676
智裕 <joaquin.it@gmail.com>
2014-06-08 13:21:26 -0700

    Update b10.variables.bash

--- bash/b10.variables.bash
+++ bash/b10.variables.bash
@@ -1,1 +1,7 @@
-#!//bin/bash
+declare -i num=10/2        # set an integer
+char=a                     # set a character
+string="This is a string"  # set a string
+# show the results
+echo Number is $num.
+echo Character is \'$char\'.
+echo String is \"$string\".
