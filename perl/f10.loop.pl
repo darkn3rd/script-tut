6328171df9844bca49289c7a414cf6ecf56e0ebe
智裕 <joaquin.it@gmail.com>
2014-06-08 12:19:49 -0700

    Update f10.loop.pl

--- perl/f10.loop.pl
+++ perl/f10.loop.pl
@@ -1,1 +1,7 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+# do...until construction
+do {
+  print "Enter your name (quit to exit): ";
+  chomp($answer = <>);
+  print "Hello $answer\n" if ($answer ne "quit");
+} until ($answer eq "quit")
