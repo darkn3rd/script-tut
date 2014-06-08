8c53b74e843e6be1b095c99363eb7c64a79624b8
지훈 <sauvecali@yahoo.com>
2014-06-08 12:35:43 -0700

    perl var

--- perl/b00.variables.pl
+++ perl/b00.variables.pl
@@ -3,6 +3,6 @@ my $num=5;
 my $chr='a';
 my $str="This is a string";
   
-print "Number is $num\n";
-print "Character is \'$chr\'\n";
-print "String is \"$str\"\n";
+print "Number is " .  $num . ".\n";
+print "Character is '" . $chr . "'.\n";
+print "String is \"" . $str . "\".\n";
--- perl/b11.variables.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-my $num=5;
-my $chr='a';
-my $str="This is a string";
-  
-print "Number is $num\n";
-print "Character is '$chr'\n";
-print "String is \"$str\"\n";
