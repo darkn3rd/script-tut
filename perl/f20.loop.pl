40d17c28e8b72d0eb29ac09d0bc0dcf1dab0884f
智裕 <joaquin.it@gmail.com>
2014-06-08 12:20:42 -0700

    Update f20.loop.pl

--- perl/f20.loop.pl
+++ perl/f20.loop.pl
@@ -1,1 +1,8 @@
-#!/usr/bin/perl
+# collection loop on list returned by subshell
+foreach $item (split /\s+/, `ls`) {
+  if (-d $item) {
+    print "$item is a directory\n"
+  } else {
+    print "$item is not a directory\n"
+  }
+}
