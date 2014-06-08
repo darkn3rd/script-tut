a93e1c988162e8778b3cbf0093ed963f79deb259
智裕 <joaquin.it@gmail.com>
2014-06-08 13:26:06 -0700

    Update f20.loop.bash

--- bash/f20.loop.bash
+++ bash/f20.loop.bash
@@ -1,1 +1,9 @@
-#!//bin/bash
+#!/bin/bash
+# loop through listing with for/in/do...done
+for item in $(ls); do                  # cycle through directory listing
+   if [[ -d $item ]]; then             # test if path is directory
+      echo "$item is a directory"
+   else
+      echo "$item is not a directory"
+   fi
+done
