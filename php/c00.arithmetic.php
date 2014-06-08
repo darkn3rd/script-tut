a5d9020cec838f04acb5fcb5b85cb230221b3164
智裕 <joaquin.it@gmail.com>
2014-06-08 11:01:27 -0700

    Update c00.arithmetic.php

--- php/c00.arithmetic.php
+++ php/c00.arithmetic.php
@@ -1,1 +1,11 @@
 #!/usr/bin/php
+<?php
+// integer arithmatic
+$width = 5; $length = 6;
+$area = $width * $length;
+echo "The area of a square(width=$width, length=$length) is $area\n";
+ 
+// bolean logic
+$result = TRUE && FALSE || TRUE;
+echo "The statement (true AND false OR true) is $result\n";
+?>
