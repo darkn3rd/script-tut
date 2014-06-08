51128b86491f11d94ca1a969c7db095a5413d95b
智裕 <joaquin.it@gmail.com>
2014-06-08 11:07:36 -0700

    Update g00.array.php

--- php/g00.array.php
+++ php/g00.array.php
@@ -1,1 +1,13 @@
 #!/usr/bin/php
+<?php
+$nicknames[0]=bob;
+$nicknames[1]=ed;
+$nicknames[2]=steve;
+$nicknames[3]=ralph;
+$nicknames[4]=joe;
+$nicknames[5]=deb;
+$nicknames[6]=kate;
+ 
+echo "The total nicknames are: ", count($nicknames), "\n";
+echo "The nicknames are: ", implode(" ", $nicknames), "\n";
+?>
