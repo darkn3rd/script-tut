5fde2ee3a637acd775942d1a5255c8c8e7c39b3c
智裕 <joaquin.it@gmail.com>
2014-06-08 12:10:44 -0700

    Update c00.arithmetic.pl

--- perl/c00.arithmetic.pl
+++ perl/c00.arithmetic.pl
@@ -1,1 +1,10 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+# integer arithmatic
+my ($width, $length) = (5,6);
+my $area=$length * $width;
+print "The area of a square (width=$width, length=$length) is $area.\n";
+  
+# boolean logic
+my ($true, $false) = (1,0);
+my $result=$true && $false || $true;
+print "The statement (true AND false OR true) is $result.\n";
