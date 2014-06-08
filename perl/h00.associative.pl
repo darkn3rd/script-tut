0679b9c899b3ec327bf4948a582130871959ec22
智裕 <joaquin.it@gmail.com>
2014-06-08 12:28:58 -0700

    Update h00.associative.pl

--- perl/h00.associative.pl
+++ perl/h00.associative.pl
@@ -1,1 +1,15 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+use strict;
+my %ages;           # declare hash
+  
+# add key/value one by one
+$ages{bob}=34;     
+$ages{ed}=58;
+$ages{steve}=32;
+$ages{ralph}=23;
+$ages{deb}=46;
+$ages{kate}=19;
+  
+# print keys and values
+print "Keys (names): ", join (" ", keys %ages), "\n";
+print "Values (ages): ", join (" " , values %ages), "\n";
