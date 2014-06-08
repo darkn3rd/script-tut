cf8efb7614c402178b94828372e22988278307fa
智裕 <joaquin.it@gmail.com>
2014-06-08 11:14:44 -0700

    Update b10.variables.php

--- php/b10.variables.php
+++ php/b10.variables.php
@@ -5,6 +5,6 @@ $chr = 'a';
 $str = "This is a string";
  
 echo "Number is " . $num . ".\n";
-echo "Character is '" . $chr" . '.\n";
+echo "Character is '" . $chr . "'.\n";
 echo "String is \"" . $str . "\".\n";
 ?>
