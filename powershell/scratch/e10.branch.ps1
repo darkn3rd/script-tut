12a5e98581c0ece81d179c2261dacd7d0f7c740d
darkn3rd <suavecali@yahoo.com>
2014-06-08 13:40:15 -0700

    powershell

---
+++ powershell/scratch/e10.branch.ps1
@@ -0,0 +1,11 @@
+
+$keypress = Read-Host "Input a character: " # prompt user and get input
+$keypress = $keypress.substring(0,1)        # substring for only 1st char
+
+# switch w/ regexp construction
+switch -regex -casesensitive ($keypress) {
+ "[:lower:]" { "Lowercase letter"}
+ "[:upper:]" { "Uppercase letter"}
+ "[:digit:]" { "Digit" }
+ default { "Punctuation, whitespace, or other" }
+}
