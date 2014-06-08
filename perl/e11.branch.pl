eacfb2874f95644c70a2e376986ab8830b2a4616
智裕 <joaquin.it@gmail.com>
2014-06-08 12:13:38 -0700

    Update e11.branch.pl

--- perl/e11.branch.pl
+++ perl/e11.branch.pl
@@ -1,1 +1,11 @@
-s
+#!/usr/bin/perl -w
+use v5.14;
+  
+print "Input a character: "; my $keypress=getc(STDIN);
+  
+for ($keypress) {
+  print "Lowercase letter\n" when /[[:lower:]]/;
+  print "Uppercase letter\n" when /[[:upper:]]/;
+  print "Digit\n" when /[[:digit:]]/;
+  default { print "Punctuation, whitespace, or other\n"; }
+}
