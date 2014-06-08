c1bbf632263d275c6c61314c58c31302c8ab0bc5
智裕 <joaquin.it@gmail.com>
2014-06-08 12:12:42 -0700

    Update e10.branch.pl

--- perl/e10.branch.pl
+++ perl/e10.branch.pl
@@ -1,1 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+use v5.10.1;
+print "Input a character: "; $keypress=getc(STDIN);
+  
+# given construction
+given ($keypress) {
+  when (/[[:lower:]]/) { print "Lowercase letter\n"; }
+  when (/[[:upper:]]/) { print "Uppercase letter\n"; }
+  when (/[[:digit:]]/) { print "Digit\n" }
+  default { print "Punctuation, whitespace, or other\n"; }
+}
