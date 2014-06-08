050e44541d4cf7bd5821dab0e1285c4f70d3e208
智裕 <joaquin.it@gmail.com>
2014-06-08 13:24:21 -0700

    Update e10.branch.bash

--- bash/e10.branch.bash
+++ bash/e10.branch.bash
@@ -1,1 +1,11 @@
-#!//bin/bash
+#!/bin/bash
+read -p "Input a character: " keypress # prompt user and get input
+keypress=${keypress:0:1}               # substring for only 1st char
+ 
+# case construction
+case "$keypress" in
+  [[:lower:]] ) echo "Lowercase letter";;
+  [[:upper:]] ) echo "Uppercase letter";;
+  [[:digit:]] ) echo "Digit";;
+            * ) echo "Punctuation, whitespace, or other";;
+esac
