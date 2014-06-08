23ca20f59e0b3094acb295106e6e6a87dc0e6f1c
darkn3rd <suavecali@yahoo.com>
2014-06-08 14:03:54 -0700

    jscript

---
+++ jscript/deceased/g00.array.js
@@ -0,0 +1,21 @@
+// create empty list
+var nicknames = new Array()
+// resize array and insert element by index
+nicknames[0] = "bob"
+nicknames[1] = "ed"
+nicknames[2] = "steve"
+nicknames[3] = "ralph"
+nicknames[4] = "joe"
+nicknames[5] = "deb"
+nicknames[6] = "kate"
+
+WScript.echo("The total nicknames are: " + nicknames.length);
+WScript.stdout.write("The nicknames are: ");
+
+// JScript cannot enumerate Array, so must do manually
+for (var index in nicknames) { 
+  if (index == nicknames.length - 1) 
+    WScript.Echo(nicknames[index]);
+  else 
+    WScript.stdout.write(nicknames[index] + ", ");
+}
