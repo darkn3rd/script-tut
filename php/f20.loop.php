1e3567a87b9b763f5e4923efab4079ba110741eb
智裕 <joaquin.it@gmail.com>
2014-06-08 11:07:05 -0700

    Update f20.loop.php

--- php/f20.loop.php
+++ php/f20.loop.php
@@ -1,1 +1,11 @@
 #!/usr/bin/php
+<?php
+// foreach (as) construction
+foreach (preg_split("/\s+/", shell_exec('ls')) as $item) {
+   if (is_dir($item)) {
+       echo "$item is a directory\n";
+   } else {
+       echo "$item is not a directory\n";
+   }
+}
+?>
