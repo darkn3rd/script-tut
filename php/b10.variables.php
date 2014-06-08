246145d7db731a8c4240996f316df90a30ea1281
智裕 <joaquin.it@gmail.com>
2014-06-08 11:15:22 -0700

    Update b10.variables.php

--- php/b10.variables.php
+++ php/b10.variables.php
@@ -1,9 +1,11 @@
 #!/usr/bin/php
 <?php
+# declare variables 
 $num = 5;
 $chr = 'a';
 $str = "This is a string";
- 
+
+# coutput results using string concatenation 
 echo "Number is " . $num . ".\n";
 echo "Character is '" . $chr . "'.\n";
 echo "String is \"" . $str . "\".\n";
