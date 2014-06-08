66ba6bba465731ddfbe7ccde8d2f05bd09eafadc
智裕 <joaquin.it@gmail.com>
2014-06-08 11:04:07 -0700

    Update e10.branch.php

--- php/e10.branch.php
+++ php/e10.branch.php
@@ -1,1 +1,18 @@
 #!/usr/bin/php
+<?php
+echo "Input a character: "; $Keypress = fgetc(STDIN);
+ 
+switch (TRUE) {
+  case preg_match("/[[:lower:]]/", $Keypress):
+    echo "Lowercase letter\n";
+    break;
+  case preg_match("/[[:upper:]]/", $Keypress):
+    echo "Uppercase letter\n";
+    break;
+  case preg_match("/[[:digit:]]/", $Keypress):
+    echo "Digit\n";
+    break;
+  default:
+    echo "Puncuation, whitespace, or other\n";
+}
+?>
