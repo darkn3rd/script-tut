dcbe4ab3e6593d5e324618f8cae39277a1e08e7b
智裕 <joaquin.it@gmail.com>
2014-06-08 12:11:31 -0700

    Update d00.input.pl

--- perl/d00.input.pl
+++ perl/d00.input.pl
@@ -1,1 +1,5 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+print "Enter your name: "; # print prompt
+my $name=<>;               # acquire string input including newline 
+chomp $name;               # strip newline 
+print "Hello $name"        # output result using variable
