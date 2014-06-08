87d717a9efd5b1617da90ae0e61f3421e4788436
智裕 <joaquin.it@gmail.com>
2014-06-08 11:04:39 -0700

    Update g20.array.php

--- php/g20.array.php
+++ php/g20.array.php
@@ -1,1 +1,14 @@
 #!/usr/bin/php
+<?php
+echo "Input a character: "; $Keypress = fgetc(STDIN);
+ 
+if (preg_match("/[[:lower:]]/", $Keypress)) {
+    echo "Lowercase letter\n";
+} elseif (preg_match("/[[:upper:]]/", $Keypress)) {
+    echo "Uppercase letter\n";
+} elseif (preg_match("/[[:digit:]]/", $Keypress)) {
+    echo "Digit\n";
+} else {
+    echo "Punctuation, whitespace, or other\n";
+}
+?>
