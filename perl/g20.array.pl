81cd1a66a158524718fa68b6a9ffe567f82b5e67
智裕 <joaquin.it@gmail.com>
2014-06-08 12:28:34 -0700

    Update g20.array.pl

--- perl/g20.array.pl
+++ perl/g20.array.pl
@@ -1,1 +1,6 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+my @nicknames=qw(bob ed steve ralph joe deb kate);
+print "The names are: \n";
+for (my $count=0; $count < scalar(@nicknames); $count++) {
+  print " nicknames[$count]=$nicknames[$count]\n"
+} 
