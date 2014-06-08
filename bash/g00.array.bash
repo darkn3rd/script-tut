47c23c42d7fde53161e1f5ad27fb5e7679324170
智裕 <joaquin.it@gmail.com>
2014-06-08 13:26:26 -0700

    Update g00.array.bash

--- bash/g00.array.bash
+++ bash/g00.array.bash
@@ -1,1 +1,12 @@
-#!//bin/bash
+#!/bin/bash
+# assign items to array
+nicknames[0]=bob
+nicknames[1]=ed
+nicknames[2]=steve
+nicknames[3]=ralph
+nicknames[4]=joe
+nicknames[5]=deb
+nicknames[6]=kate
+ 
+echo "The number of nicknames are: ${#nicknames[*]}"  # print lenght of array
+echo "The nicknames are: ${nicknames[*]}"             # enumerate array
