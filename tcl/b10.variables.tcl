c13bd719f80575e31fbf3fad2b895c50f398816f
지훈 <sauvecali@yahoo.com>
2014-06-13 00:52:32 -0700

    tcl vars

--- tcl/b10.variables.tcl
+++ tcl/b10.variables.tcl
@@ -1,8 +1,10 @@
 #!/usr/bin/tclsh
+# declare variables
 set num    5
 set char   a
 set string "This is a string"
  
+# output variables 
 puts "Number is $num."
 puts "Character is a '$char'."
 puts "String is \"$string\"."
