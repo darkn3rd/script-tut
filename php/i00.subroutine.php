697f94bd517bb68124ea37698758af6570b1c823
지훈 <sauvecali@yahoo.com>
2014-06-15 18:11:41 -0700

    php get_date()

--- php/i00.subroutine.php
+++ php/i00.subroutine.php
@@ -3,7 +3,7 @@
 // create subroutine
 function  show_date() {
   // WARNING: In php.ini, date.timezone must be set for this to work.
-  echo "Today is ",  strftime("%B %d, %Y"), "\n";
+  echo "Today is ",  strftime("%B %d, %Y"), ".\n";
 }
 
 show_date(); // call subroutine
