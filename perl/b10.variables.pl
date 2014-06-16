e184f79f0d7611fdcb54ea3eb0a9aaa3681cdf00
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-16 15:36:05 -0700

    perl subroutine

--- perl/b10.variables.pl
+++ perl/b10.variables.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl -w
-my $num=5;
-my $chr='a';
-my $str="This is a string";
-  
+# declare variables
+my $num = 5;
+my $chr = 'a';
+my $str = "This is a string";
+
+# user variables  
 print "Number is $num.\n";
 print "Character is '$chr'.\n";
 print "String is \"$str\".\n";
--- perl/i00.subroutine.pl
+++ perl/i00.subroutine.pl
@@ -1,1 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+use POSIX qw(strftime);  # library to get us strftime
+
+# create subroutine
+sub show_date {
+  my $date = strftime("%B %d, %Y", gmtime(time));
+  print "Today is $date.\n";
+}
+
+# call subroutine
+show_date
