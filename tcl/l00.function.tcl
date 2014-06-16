8ad0eac703cd698b5c6e0966af7b487559db3bcb
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-16 13:22:12 -0700

    tcl functions

--- tcl/l00.function.tcl
+++ tcl/l00.function.tcl
@@ -1,1 +1,18 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+
+# create function that returns integer output
+proc addNums {args} {
+  # local variables
+  set sum 0          ;# initial sum value
+  set nums $args     ;# friendlier name for arguments
+  
+  # add up all the num in nums array
+  foreach num $nums { set sum [expr $sum + $num] }
+  # output results of summation
+  return $sum
+}
+
+# call function
+set result [addNums 5 2 4 3 6]
+puts "The result of summation is: $result."
+
--- tcl/l10.function.tcl
+++ tcl/l10.function.tcl
@@ -1,1 +1,11 @@
-#!//usr/bin/tclsh
+#!/usr/bin/tclsh
+
+# create function that returns string output
+proc capitalize {string} { 
+  return [string toupper $string]
+}
+
+# call function with variable number of parameters
+set result [capitalize "ibm"]
+puts "The result of capitalization is: $result."
+
