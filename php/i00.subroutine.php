b87f5b4a9cfbe22877c99992a4250c86e3948bcf
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-16 15:38:55 -0700

    php sub update

--- php/i00.subroutine.php
+++ php/i00.subroutine.php
@@ -3,7 +3,8 @@
 // create subroutine
 function  show_date() {
   // WARNING: In php.ini, date.timezone must be set for this to work.
-  echo "Today is ",  strftime("%B %d, %Y"), ".\n";
+  $date = strftime("%B %d, %Y");
+  echo "Today is $date.\n";
 }
 
 show_date(); // call subroutine
