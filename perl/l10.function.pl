a4d24009596c7820b8bfe9fd152cdf3598b45bdd
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-16 16:11:51 -0700

    perl function

--- perl/l10.function.pl
+++ perl/l10.function.pl
@@ -1,1 +1,10 @@
 #!/usr/bin/perl
+# create function that returns a string scalar
+sub capitalize {
+  return uc($_[0]);             # return capitlized string
+}
+
+# call the function
+my $result = capitalize("ibm"); # pass string 
+# output results with resulting string
+print "The result of capitalization is: $result.\n";
