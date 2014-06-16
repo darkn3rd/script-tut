d775b6e04bb6b8a45681e4d321749342b344db25
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-16 15:59:41 -0700

    perl function

--- perl/l00.function.pl
+++ perl/l00.function.pl
@@ -1,1 +1,16 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+# create function that returns an integer
+sub addNums {
+   my @numbers = @_;   # retreive variable parameters
+   my $sum = 0;        # initalize to 0
+
+   # add all the $num in @numbers list
+   foreach $num (@numbers) { $sum += $num }              
+
+   return $sum;        # return sum of numbers
+}
+
+# call the function
+my $result = addNums 5, 2, 4, 3, 6; # pass variable number of numbers
+# output results with resulting integer
+print "The result of summation is: $result.\n";
