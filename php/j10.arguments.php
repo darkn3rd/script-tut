4ca5fb8d44bab0a43ef078b1dc2a33b8949a6443
지훈 <sauvecali@yahoo.com>
2014-06-15 19:37:41 -0700

    php arg update

--- php/j10.arguments.php
+++ php/j10.arguments.php
@@ -3,7 +3,8 @@
 // acquire num of args
 $arg_count   = $argc - 1;  // get num of arguments
 
+echo "The arguments passed are:\n";
 // iterative loop to enumerate args
 for ( $count = 1; $count <= $arg_count ; $count++ )
-  echo "item $count: $argv[$count]\n";
+  echo "  item $count: $argv[$count]\n";
 ?>
--- php/j20.arguments.php
+++ php/j20.arguments.php
@@ -3,7 +3,8 @@
 // acquire num of args
 $arg_count   = $argc - 1;  // get num of arguments
 
+echo "The arguments passed are (reverse order):\n";
 // iterative loop to enumerate args
 for ( $count = $arg_count; $count > 0; $count-- )
-  echo "item $count: $argv[$count]\n";
+  echo "  item $count: $argv[$count]\n";
 ?>
