9016b9b47b13264d402b263001f668d8734f88b3
지훈 <sauvecali@yahoo.com>
2014-06-15 20:04:30 -0700

    php functions

--- php/l00.function.php
+++ php/l00.function.php
@@ -1,1 +1,20 @@
 #!/usr/bin/php
+<?php
+// create function
+function addNums()
+{
+   $numbers = func_get_args();    // retrieve variable number of numbers
+   $sum = 0;                      // initalize to 0
+
+   // add all nums in array 
+   foreach ($numbers as $num) $sum += $num;             
+ 
+   return $sum;                   // return result
+}
+
+// call the function
+$result = addNums(5, 2, 4, 3, 6); // pass variable number of numbers
+
+# output results with resulting integer
+echo "The result of summation is: $result.\n";
+?>
--- php/l10.function.php
+++ php/l10.function.php
@@ -1,1 +1,13 @@
 #!/usr/bin/php
+<?php
+// create function
+function capitalize($string) {
+	return strtoupper($string); // return capitlized string
+}
+
+// call the function
+$result = capitalize("ibm");    // pass string 
+
+# output results with resulting integer
+echo "The result of capitalization is: $result.\n";
+?>
