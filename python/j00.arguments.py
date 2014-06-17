ce213cc2e53d5f2d0687aa3af1e40371f3c99fc6
Joaquin Menchaca <jmenchaca@Joaquins-MacBook-Air.local>
2014-06-17 10:43:41 -0700

    python args

--- python/j00.arguments.py
+++ python/j00.arguments.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import sys
 
+arg_count   = len(sys.argv) - 1 # get num of arguments
+script_name = sys.argv[0]       # get name of script
+
 # check for only 2 arguments
-if len(sys.argv) != 3:
+if arg_count != 2:
  # print helpful instructions 
- print "You need to enter two numbers: %s num1 num2" % sys.argv[0]
+ print "You need to enter two numbers: %s num1 num2" % script_name 
 else:
  # print sum pf two integer arguments
  print "The sum of %d and %d is: %d." % (int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[1]) + int(sys.argv[2]))
