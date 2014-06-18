aac4ad0ec9530d2fe5d8afe9a56587dd1c399d3f
지훈 <sauvecali@yahoo.com>
2014-06-18 01:11:47 -0700

    arg update

--- python/j00.arguments.py
+++ python/j00.arguments.py
@@ -6,11 +6,11 @@ script_name = sys.argv[0]       # get name of script
 
 # check for only 2 arguments
 if arg_count != 2:
- # print helpful instructions 
- print "\nYou need to enter two numbers: \n"
- print "   Usage: %s [num1] [num2]\n" % script_name
+   # print helpful instructions 
+   print "\nYou need to enter two numbers: \n"
+   print "   Usage: %s [num1] [num2]\n" % script_name
 else:
- # get sum of both arguments, force cast them to integer
- sum = int(sys.argv[1]) + int(sys.argv[2])  
- # print results of both arguments (string) and summation (integer)
- print "The sum of %s and %s is: %d." % (sys.argv[1], sys.argv[2], sum)
+   # get sum of both arguments, force cast them to integer
+   sum = int(sys.argv[1]) + int(sys.argv[2])  
+   # print results of both arguments (string) and summation (integer)
+   print "The sum of %s and %s is: %d." % (sys.argv[1], sys.argv[2], sum)
--- shell/j00.arguments.sh
+++ shell/j00.arguments.sh
@@ -6,8 +6,10 @@ arg_count=$#    # get number of arguments
 # test if user entered 2 values
 if [ $arg_count -ne 2 ]; then
   # print message if less than two numbers
-  echo "You need to enter two numbers: \"$script_name\" num1 num2"
+  echo "\nYou need to enter two numbers: \n"
+  echo "   Usage: $script_name [num1] [num2]\n"
 else
-  # print summation of both arguments
-  echo "The sum of $1 and $2 is: $(($1 + $2))"
+  sum=$(($1 + $2))  # get sum of both arguments
+  # print results of both arguments and summation
+  echo "The sum of $1 and $2 is: $sum"
 fi
