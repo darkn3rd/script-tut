cb29edf6ba59b486f145d369fa4da30a6dafce1a
지훈 <sauvecali@yahoo.com>
2014-06-18 00:31:19 -0700

    perl arg update

--- perl/j00.arguments.pl
+++ perl/j00.arguments.pl
@@ -1,10 +1,13 @@
 #!/usr/bin/perl -w
 # acquire num of args and script name
-$arg_count   = $#ARGV + 1; # get num of arguments
-$script_name = $0;         # get script name
+$arg_count   = $#ARGV + 1;    # get num of arguments
+$script_name = $0;            # get script name
 
 if ($arg_count != 2) {
-  print "You need to enter two numbers: \"$script_name\" num1 num2.\n";
+  print "\nYou need to enter two numbers: \n\n";
+  print "   Usage: $script_name [num1] [num2]\n\n";
 } else {
-  printf  "The sum of %d and %d is: %d.\n", $ARGV[0], $ARGV[1], $ARGV[0] + $ARGV[1];
+  $sum = $ARGV[0] + $ARGV[1]; # get sum of both arguments
+  # print results of both arguments and summation
+  printf  "The sum of %d and %d is: %d.\n", $ARGV[0], $ARGV[1], $sum;
 }
