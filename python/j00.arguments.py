f01c310046ae7033f049dfa01a0f875fca1c7718
지훈 <sauvecali@yahoo.com>
2014-06-18 00:56:46 -0700

    python arg update

--- python/j00.arguments.py
+++ python/j00.arguments.py
@@ -7,7 +7,10 @@ script_name = sys.argv[0]       # get name of script
 # check for only 2 arguments
 if arg_count != 2:
  # print helpful instructions 
- print "You need to enter two numbers: %s num1 num2" % script_name 
+ print "\nYou need to enter two numbers: \n"
+ print "   Usage: %s [num1] [num2]\n" % script_name
 else:
- # print sum pf two integer arguments
- print "The sum of %d and %d is: %d." % (int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[1]) + int(sys.argv[2]))
+ # get sum of both arguments, force cast them to integer
+ sum = int(sys.argv[1]) + int(sys.argv[2])  
+ # print results of both arguments (string) and summation (integer)
+ print "The sum of %s and %s is: %d." % (sys.argv[1], sys.argv[2], sum)
