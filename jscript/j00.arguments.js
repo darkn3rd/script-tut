f038acf6de426a8b547506789c95afc07c69f4c6
darkn3rd <suavecali@yahoo.com>
2014-06-18 01:38:28 -0700

    jscript arg update

--- jscript/j00.arguments.js
+++ jscript/j00.arguments.js
@@ -1,6 +1,5 @@
 var arg_count   = WScript.Arguments.Count() // get num of arguments
-var script_name = WScript.ScriptName      // get script name
-
+var script_name = WScript.ScriptName        // get script name
 
 // check for only two arguments
 if (WScript.Arguments.Count() != 2) {
@@ -9,7 +8,8 @@ if (WScript.Arguments.Count() != 2) {
  WScript.Echo("   Usage: " + script_name + " [num1] [num2]");
 } else {
  // get sum of both arguments, force cast to integer
- var sum = parseInt(WScript.Arguments.Item(0)) + parseInt(WScript.Arguments.Item(1));
+ var sum = parseInt(WScript.Arguments.Item(0)) + 
+           parseInt(WScript.Arguments.Item(1));
  // print results of both arguments (string) and summation (integer)
  WScript.Echo("The sum of " + WScript.Arguments.Item(0) + 
               " and " + WScript.Arguments.Item(1) + 
