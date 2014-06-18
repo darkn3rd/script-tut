0f89bc86beb18fa4a11cb3f388cef1696630cbdc
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-17 17:40:40 -0700

    posix sh input update

--- shell/d00.inut.sh
+++ shell/d00.inut.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo -n "Enter your name: " # print prompt
+printf "Enter your name: "  # print prompt
 read name                   # acquire input
-echo Hello $name            # output result using variable
+echo Hello $name!           # output result using variable
