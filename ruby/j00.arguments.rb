24092c4e2f6520adb98a9c68bd5511b56f328ff5
지훈 <sauvecali@yahoo.com>
2014-06-18 01:00:38 -0700

    ruby arg update

--- ruby/j00.arguments.rb
+++ ruby/j00.arguments.rb
@@ -5,9 +5,12 @@ script_name = $0          # get script name
 
 # check for only 2 arguments
 if arg_count != 2 then
- # print helpful instructions 
- puts "You need to enter two numbers: #{script_name} num1 num2"  
+   # print helpful instructions 
+   puts "\nYou need to enter two numbers: \n\n"
+   puts "   Usage: #{script_name} [num1] [num2]\n\n" 
 else
- # print sum pf two integer arguments
- puts "The sum of #{ARGV[0]} and #{ARGV[1]} is: #{ARGV[0].to_i + ARGV[1].to_i}"
+   # get sum of both arguments, force cast them to integer
+   sum = ARGV[0].to_i + ARGV[1].to_i
+   # print results of both arguments (string) and summation (integer)
+   puts "The sum of #{ARGV[0]} and #{ARGV[1]} is: #{sum}"
 end
