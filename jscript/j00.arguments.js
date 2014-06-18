6ae4c87e9d151dc2832e6a4ccb30f71631dda7e0
darkn3rd <suavecali@yahoo.com>
2014-06-18 01:36:59 -0700

    jscript arg update

--- jscript/j00.arguments.js
+++ jscript/j00.arguments.js
@@ -7,7 +7,6 @@ if (WScript.Arguments.Count() != 2) {
  // print helpful instructions
  WScript.Echo("\nYou need to enter two numbers:\n");
  WScript.Echo("   Usage: " + script_name + " [num1] [num2]");
-
 } else {
  // get sum of both arguments, force cast to integer
  var sum = parseInt(WScript.Arguments.Item(0)) + parseInt(WScript.Arguments.Item(1));
