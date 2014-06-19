cdafe4d41bf01fd23be42f57f0020c99bb9dd613
Joaquin Menchaca <jmenchaca@Joaquins-MacBook-Air.local>
2014-06-18 19:36:31 -0700

    shell comment update

--- shell/j21.arguments.sh
+++ shell/j21.arguments.sh
@@ -10,7 +10,7 @@ for count in $(seq 1 $arg_count); do
   arg=$1                    # get arg from first positional
   # build ouput string by prepending previous result
   output="  item $count: $arg\n$output" 
-  shift                             # shift positionals by one
+  shift                     # shift positionals by one
 done
 
 # output resulting compiled string
