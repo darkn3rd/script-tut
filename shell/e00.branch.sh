5b6e762f7f89caa9bf2ff254b9c83e5f02b3593a
Joaquin Menchaca <jmenchaca@Joaquins-MacBook-Air.local>
2014-06-19 23:55:43 -0700

    shell echo -n to prinf

--- shell/e00.branch.sh
+++ shell/e00.branch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo -n "Input a number: "; read Number
-if [ $Number -gt 0 ]; then
-  echo Number is greater than 0
-elif [ $Number -lt 0 ]; then
-  echo Number is less than 0
+printf "Input a number: "; read number
+if [ $number -gt 0 ]; then
+  echo number is greater than 0
+elif [ $number -lt 0 ]; then
+  echo number is less than 0
 else
-  echo Number is 0
+  echo number is 0
 fi
