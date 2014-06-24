fd5170815b4d8f2c1503d7d574e33b53d0878e0f
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-23 19:40:51 -0700

    ruby loop comments

--- ruby/f10.loop.rb
+++ ruby/f10.loop.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 # count style loop with times iterator
 10.times do |count|
-  puts "Count is #{(count-10)*-1}"
+  # output result, some math used reverse number
+  puts "Count is #{(count-10)*-1}"  
 end
