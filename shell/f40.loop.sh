4133a32ebf1cec9fae7e3c7a36bc735c840192d4
Joaquin Menchaca <jmenchaca@Joaquins-MacBook-Air.local>
2014-06-24 20:49:51 -0700

    shell loop skip in progress

--- shell/f40.loop.sh
+++ shell/f40.loop.sh
@@ -5,9 +5,15 @@ while [ 1 ]; do
    printf "Enter your name (quit to exit): "
    read answer         # get input
    
-   # exit loop if user wants to quit
-   if [ $answer = "quit" ] ; then
-     break   
+   
+   if [  $(echo "$answer" | grep '^[[:blank:]]*$') ]; then
+     echo "got blank"
+     #continue          # skip loop if no answer
+   fi
+   
+   
+   if [ "$answer" = "quit" ] ; then
+     break             # exit loop if user wants to quit
    fi
    
    echo Hello $answer! # output result as exiting
