36fa28a55cb4b4e8fc453941bd008a7db4de1658
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-25 11:10:06 -0700

    shell loop skip in scratch

--- shell/scratch/f40.sh
+++ shell/scratch/f40.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
+# utility variables
+true=1
+false=0
 # spin loop with break used to exit from loop
-while [ 1 ]; do
+while [ $true ]; do
    # output prompt using external printf command
    printf "Enter your name (quit to exit): "
    read answer         # get input
+  
+   # search for blank result and save state as error code
+   #   error codes
+   #     0 = match found
+   #     1 = no match found
+   echo "$answer" | grep -q "^[[:blank:]]*$"
+   # save error code for later use
+   isError=$?
+
+   # test error code from previous match
+   if [ $isError = $false ]; then
+     continue          # skip loop if no answer
+   fi
    
-   if echo "$answer" | grep -q '^[[:blank:]]*$'; then continue; fi
-   
-   if [ "$answer" = "quit" ] ; then break; fi
+   if [ "$answer" = "quit" ] ; then
+     break             # exit loop if user wants to quit
+   fi
    
    echo Hello $answer! # output result as exiting
 done
