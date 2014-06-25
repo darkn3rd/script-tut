8d5360425516d62dd962aeebbe37790cc2e2f538
Joaquin Menchaca <jmenchaca@Joaquins-MacBook-Air.local>
2014-06-24 20:13:57 -0700

    php loop skip

--- php/f40.loop.php
+++ php/f40.loop.php
@@ -5,6 +5,8 @@
 do {
     echo "Enter your name (quit to Exit): "; # output prompt
     $answer = rtrim(fgets(STDIN));           # get input, trim newline
+    # skip loop if not answer entered
+    if (preg_match("/^[\s\t]*$/", $answer)) continue; 
     if ($answer == "quit") break;            # exit loop if user quits                           
     echo "Hello $answer!\n";                 # output results as not exiting
 } while (1);
