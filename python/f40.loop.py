b4484d08b54388fa3c261a5786153b79d73a38aa
Joaquin Menchaca <jmenchaca@Joaquins-MacBook-Air.local>
2014-06-24 20:13:38 -0700

    python loop skip

--- python/f40.loop.py
+++ python/f40.loop.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
+import re
 # spin loop as always true condition
 #   break to exit from loop
 while True:
   # print a prompt and get answer
   answer = raw_input("Enter your name (quit to Exit): ")
+  # skip loop if user enters an empty string
+  if re.compile("^[\s\t]*$").match(answer):
+    continue
   if answer == "quit":
-    break                      # exit loop as user wants to quit
+    break                      # exit loop if user wants to quit
   print "Hello %s!" % answer   # output result as we are not exiting
