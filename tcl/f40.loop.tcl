c698d15707d7292c6854ed65916ada0ab269521c
Joaquin Menchaca <jmenchaca@Joaquins-MacBook-Air.local>
2014-06-24 20:26:14 -0700

    tcl loop skip

--- tcl/f40.loop.tcl
+++ tcl/f40.loop.tcl
@@ -1,11 +1,13 @@
 #!/usr/bin/tclsh
-# spin loop with break to exit loop
+# spin loop with break to exit loop, continue to skip loop
 while {true} {
   # send prompt to buffer
   puts -nonewline "Enter you name (quit to Exit): "
   flush stdout         ;# output the buffer
   gets stdin answer    ;# get intput
-  
+ 
+  # skip loop if no answer entered
+  if {[regexp {^[\s\t]*$} $answer]} { continue }
   # exit loop if user quits
   if {$answer == "quit"} { break } 
   
