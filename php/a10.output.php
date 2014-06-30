5f7180db23c202efdcd74eccc64ee05049b45453
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-30 14:38:11 -0700

    php stderr snippet

---
+++ php/a10.output.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?php
+# output message to standard error
+#  Note: Test by redirecting stdout to nowhere, e.g.
+#   script > /dev/null
+fwrite(STDERR, "This is a test of the emergency script system.  This is only a test.\n")
+?>
+
