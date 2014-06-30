66c58a90164abd9ac232166e026a6404fc5e6b2d
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-30 14:39:06 -0700

    (k|ba)sh stderr snippet

---
+++ bash/a10.output.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+# output message to standard error
+#  Note: Test by redirecting stdout to nowhere, e.g.
+#   script > /dev/null
+echo "This is a test of the emergency script system.  This is only a test." >&2
---
+++ ksh/a10.output.ksh
@@ -0,0 +1,2 @@
+#!/bin/ksh
+print -u2 "This is a test of the emergency script system.  This is only a test"
