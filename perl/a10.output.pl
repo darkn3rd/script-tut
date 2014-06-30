73a5dd341f9256de341308e127b315ad41e2beb9
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-30 14:19:41 -0700

    perl stderr snippet

---
+++ perl/a10.output.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl -w
+# output message to standard error
+#  Note: Test by redirecting stdout to nowhere, e.g.
+#   script > /dev/null
+print STDERR "This is a test of the emergency script system.  This is only a test.\n"
