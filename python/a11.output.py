85c39bcac0fe996a950a4751ffe1c97aa26ca771
Joaquin Menchaca <jmenchaca@host-253-6-0-10.sfoffice.verticalresponse.com>
2014-06-30 14:26:18 -0700

    python stderr snippet

---
+++ python/a11.output.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+import sys
+# output message to standard error
+#  Note: Test by redirecting stdout to nowhere, e.g.
+#   script > /dev/null
+sys.stderr.write("This is a test of the emergency script system.  This is only a test.\n")
