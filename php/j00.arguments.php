0da63c63aee6830b7d13a8bee337d4ef3243c1bf
Joaquin Menchaca <jmenchaca@verticalresponse.com>
2014-07-02 16:59:34 -0700

    php stderr update

--- php/j00.arguments.php
+++ php/j00.arguments.php
@@ -5,8 +5,9 @@ $arg_count   = $argc - 1;  // get num of arguments
 $script_name = $argv[0];   // get script name
 
 if ($arg_count != 2) {
-   echo "\nYou need to enter two numbers: \n\n";
-   echo "   Usage: $script_name [num1] [num2]\n\n";
+   # output usage statment to standard error
+   fwrite(STDERR, "\nYou need to enter two numbers: \n\n");
+   fwrite(STDERR, "   Usage: $script_name [num1] [num2]\n\n");
 } else {
   $sum = $argv[1] + $argv[2]; // get sum of both arguments
   // print results of both arguments and summation
