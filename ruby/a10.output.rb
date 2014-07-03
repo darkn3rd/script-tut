a51fdb7dc64af3daf03dc9ef2d48b29c6b89123f
Joaquin Menchaca <jmenchaca@verticalresponse.com>
2014-07-02 17:06:44 -0700

    ruby stderr update

--- ruby/a10.output.rb
+++ ruby/a10.output.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/ruby
-$stderr.puts "This is a test of the emergency script system.  This is only a test"
+$stderr.puts "This is a test of the emergency script system." +
+             "  This is only a test"
--- ruby/j00.arguments.rb
+++ ruby/j00.arguments.rb
@@ -5,9 +5,9 @@ script_name = $0          # get script name
 
 # check for only 2 arguments
 if arg_count != 2 then
-   # print helpful instructions 
-   puts "\nYou need to enter two numbers: \n\n"
-   puts "   Usage: #{script_name} [num1] [num2]\n\n" 
+   # output usage statement to standard error
+   $stderr.puts "\nYou need to enter two numbers: \n\n"
+   $stderr.puts "   Usage: #{script_name} [num1] [num2]\n\n" 
 else
    # get sum of both arguments, force cast them to integer
    sum = ARGV[0].to_i + ARGV[1].to_i
