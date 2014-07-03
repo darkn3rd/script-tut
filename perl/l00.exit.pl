f6b72292cfe9f82059a248f95444d16b02df29f0
Joaquin Menchaca <jmenchaca@verticalresponse.com>
2014-07-03 11:41:50 -0700

    perl exit snippet

---
+++ perl/l00.exit.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+# acquire num of args and script name
+$arg_count   = $#ARGV + 1; # get num of arguments
+$script_name = $0;         # get script name
+$EX_USAGE    = 64;         # status for command line usage error
+$EX_OK       = 0;          # status for successful termination
+
+# check for correct number of arguments
+if ($arg_count < 2) {
+	usage_message();         # output usage message
+} else {
+	addNums(@ARGV)           # process arguments
+}
+
+# output a usage message
+sub usage_message {
+  # output usage statement to standard error
+  print STDERR "\nYou need to enter two or more numbers: \n\n";
+  print STDERR "   Usage: $script_name [num1] [num2] [num3]...\n\n";
+  exit $EX_USAGE;          # return error code
+}
+
+# create subroutine with variable parameters
+sub addNums {
+   my @numbers = @_;       # retreive variable parameters
+   my $sum = 0;            # initalize to 0
+
+   # add all the $num in @numbers list
+   foreach $num (@numbers) { $sum += $num }              
+     
+   # output results
+   print "The summation is: $sum\n";
+   exit $EX_OK;            # return a success
+}
