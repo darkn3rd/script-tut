9db688726b61f7f38d1b5ac46627fc6878fa1b5e
지훈 <sauvecali@yahoo.com>
2014-07-03 20:14:17 -0700

    python exit

--- python/l00.exit.py
+++ python/l00.exit.py
@@ -5,14 +5,15 @@ import sys
 arg_count   = len(sys.argv) - 1 # get num of real arguments
 script_name = sys.argv[0]       # get name of script
 
+# subroutine to output usage message to stderr
 def usage_message ():
    sys.stderr.write("\nYou need to enter two or more numbers: \n\n")
    sys.stderr.write("   Usage: %s [num1] [num2] [num3]...\n\n" % script_name)
 
-# create method (subroutine)
+# subroutine to sum numbers from array
 def addNums (numbers):
    sum = 0                      # initialize starting sum value
-   for num in numbers:          # iterate
+   for num in numbers:          # collection loop toprocess numbers
      sum += int(num)            # sum up nums
    print "The summation is: %d" % sum # output results
 
@@ -21,5 +22,5 @@ if arg_count < 1:
    # ouptut usage statement to standard error
    usage_message()
 else:
-   # get sum of both arguments, force cast them to integer
+   # call addNums to process arguments
    addNums(sys.argv[1:])
