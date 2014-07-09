e13fa7d7c3b4eb360c1e55f8f753e8da6afe941d
지훈 <sauvecali@yahoo.com>
2014-07-09 03:30:07 -0700

    ruby exit status code snippet update

--- ruby/l00.exit.rb
+++ ruby/l00.exit.rb
@@ -1,26 +1,42 @@
 #!/usr/bin/ruby
-
+# illustrative variables
 arg_count    = ARGV.length # get num of args
 @script_name = $0          # get script name
+EX_USAGE     = 64          # status for command line usage error
+EX_OK        = 0           # status for successful termination
 
-# addNums method (subroutine)
-def addNums (numbers)
+# **************************************
+# add_nums: Output summation of all numbers passed by caller.
+#
+# numbers  - The Array of numbers to be processed
+# **************************************
+def add_nums (numbers)
    sum = 0                               # initialize starting sum value
    numbers.each {|num| sum += num.to_i } # sum up nums
    puts "The summation is: #{sum}"       # output results
 end
 
-# addNums method (subroutine)
+# **************************************
+# usage_message: Output instructions to utilize script to standard error
+#   and then exit program with appropriate POSIX error code.
+# **************************************
 def usage_message ()
   # output usage statement to standard error
   $stderr.puts "\nYou need to enter one or more numbers: \n\n"
   $stderr.puts "   Usage: #{@script_name} [num1] [num2] [num3]...\n\n"
+
+  # exit program with appropriate error code
+  exit EX_USAGE
 end
 
-# check for only 2 arguments
+# **************************************
+#             main section 
+# **************************************
 if arg_count < 1 then
    usage_message
 else
-   addNums(ARGV)
+   add_nums ARGV
 end
 
+# exit program with success (default behavior)
+exit EX_OK
