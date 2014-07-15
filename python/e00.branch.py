ce3a27ced4c7ae532254ad5f75f1c6118d97fb61
יְהוֹיָקִים <suavecali@yahoo.com>
2014-07-14 17:18:51 -0700

    python branch update

---
+++ python/e00.branch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# get input from user
+response = raw_input("Would you like a toast? [Yes/No]: ")
+# set response string using if/else construction
+if response == "Yes":
+  response_string = "That's great"
+else:
+  response_string = "How about a muffin?"
+# output the response string
+print response_string
---
+++ python/e10.branch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# get input from user
+response = raw_input("Would you like a toast? [Yes/No]: ")
+# set response string using one line
+response_string = "That's great" if response == "Yes" else "How about a muffin?"
+# output the response string
+print response_string
