3c0dc50eb20f3cf5536e57639cc55d4626459d55
יְהוֹיָקִים <suavecali@yahoo.com>
2014-07-15 16:28:11 -0700

    function update

--- python/m20.function.py
+++ python/m20.function.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
-# return list attempt
+# sort function that returns a sorted array
+def sort_array (array):
+   return sorted(array)  # return list
 
-# input unsorted list, return sorted list
+# initialize initial array (list)
+array = ["bob","ed","steve","ralph","joe","deb","kate"]
+# output current list before calling function
+print "Current names are: ", array
+
+# initialize new array from output of function
+result = sort_array(array)
+print "Sorted names are:  ", result
