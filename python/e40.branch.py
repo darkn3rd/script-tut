357a3b23f70245f6487682dda8c90745fec22763
יְהוֹיָקִים <suavecali@yahoo.com>
2014-07-14 17:06:36 -0700

    python update

---
+++ python/e40.branch.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+number = int(raw_input("Input a number: "))
+ 
+if number > 0:
+  print "Number is greater than 0"
+elif number < 0:
+  print "Number is less than 0"
+else:
+  print "Number is 0"
