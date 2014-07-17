0e58d1be7e62d9f9ccee7b31f3b8c4a5bcbb7c0d
지훈 <sauvecali@yahoo.com>
2014-07-17 11:38:03 -0700

    python branch update

--- python/e31.branch.py
+++ python/e31.branch.py
@@ -16,26 +16,26 @@ Select an item from the menu.
 Make your selection: """
  
 # get user output 
-sys.stdout.write(menu)              # output menu and prompt
-selection = int(sys.stdin.read(1))  # read one number
+sys.stdout.write(menu)         # output menu and prompt
+selection = sys.stdin.read(1)  # read one number
 
-# NOTE: This explicitly uses integers to illustrate comparing numbers
-#  However, if user enters a non-integer, script will halt.
+# NOTE: This script DOES NOT compare integers, but rather strings
+#  of a number.  This will therefore capture all invalid entries.
 
 # test selection to matching number
-if selection == 1:
+if selection == "1":
   print "You selected a Coffee"
-elif selection == 2:
+elif selection == "2":
   print "You selected an Espresso"
-elif selection == 3:
+elif selection == "3":
   print "You selected a Latte"
-elif selection == 4:
+elif selection == "4":
   print "You selected a Machiato"
-elif selection == 5:
+elif selection == "5":
   print "You selected a Capucino"
-elif selection == 6:
+elif selection == "6":
   print "You select a Mocha"
-elif selection == 7:
+elif selection == "7":
   print "You selected a Tea"
 else:
   print "You have not entered a valid selection"
