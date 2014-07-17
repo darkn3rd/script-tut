950a994bb2d491b738ee209d67f8180be06bd397
지훈 <sauvecali@yahoo.com>
2014-07-17 12:19:43 -0700

    bash branch update

--- bash/e60.branch.bash
+++ bash/e60.branch.bash
@@ -3,11 +3,11 @@ read -p "Input a character: " keypress # prompt & get input
 keypress=${keypress:0:1}               # substring for only 1st char
  
 # test if keypress matches pattern
-if [[ $keypress = [[:lower:]] ]]; then
+if [[ $keypress = [a-z] ]]; then
   echo "Lowercase letter"
-elif [[ $keypress = [[:upper:]] ]]; then
+elif [[ $keypress = [A-Z] ]]; then
   echo "Uppercase letter"
-elif [[ $keypress = [[:digit:]] ]]; then
+elif [[ $keypress = [0-9] ]]; then
   echo "Digit"
 else
   echo "Punctuation, whitepace, or other"
