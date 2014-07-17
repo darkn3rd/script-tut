9139097fa67301143920c1608e7c77677dd816ca
지훈 <sauvecali@yahoo.com>
2014-07-17 10:39:03 -0700

    bash branch update

--- bash/e20.branch.bash
+++ bash/e20.branch.bash
@@ -1,14 +1,11 @@
 #!/bin/bash
-read -p "Input a character: " keypress # prompt & get input
-keypress=${keypress:0:1}               # substring for only 1st char
- 
-# test if keypress matches pattern
-if [[ $keypress = [a-z]; then
-  echo "Lowercase letter"
-elif [[ $keypress = [A-Z] ]]; then
-  echo "Uppercase letter"
-elif [[ $keypress = [0-9] ]]; then
-  echo "Digit"
+# prompt user and get input
+read -p "Input a number: " number
+# test input and output result
+if (( $number > 0 )); then
+  echo Number is greater than 0
+elif (( $number < 0 )); then
+  echo Number is less than 0
 else
-  echo "Punctuation, whitepace, or other"
+  echo Number is 0
 fi
---
+++ bash/l00.quit.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+# illustrative variables
+SCRIPT_NAME=$0  # get name of script
+ARG_COUNT=$#    # get number of arguments
+
+# test if user entered 2 values
+if (( $ARG_COUNT != 2 )); then
+  # output usage statement to standard error
+  printf "\nYou need to enter two numbers: \n\n" >&2
+  printf "   Usage: $SCRIPT_NAME [num1] [num2]\n\n" >&2
+else
+  sum=$(($1 + $2))  # get sum of both arguments
+  # print results of both arguments and summation
+  echo "The sum of $1 and $2 is: $sum"
+fi
