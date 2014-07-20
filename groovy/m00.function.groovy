21bfdab42f63ece2f8335694aac104a2e671354d
지훈 <sauvecali@yahoo.com>
2014-07-20 00:27:20 -0700

    groovy function

--- groovy/m00.function.groovy
+++ groovy/m00.function.groovy
@@ -2,16 +2,15 @@
 // create the function
 def addNums (Object[] numbers) {
    sum = 0                            // initialize starting sum value
-   for (num in numbers)                // iterate
-      sum += num                      // sum up nums
-   sum                         // return the sum
+   numbers.each { num -> sum += num } // iterate and sum up nums
+   sum                                // return the sum
 }
 
 // output numbers before function
 println "The numbers to be added are ${[5, 2, 4, 3, 6]}."
 
 // call the function
-result = addNums(5, 2, 4, 3, 6)
+result = addNums 5, 2, 4, 3, 6
 
 // output results
 println "The result of their summation is: $result."
