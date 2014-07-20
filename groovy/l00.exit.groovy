8e3af3dc6ab9d2fdb9bea62e0485570331632751
지훈 <sauvecali@yahoo.com>
2014-07-20 12:33:02 -0700

    groovy exit

--- groovy/l00.exit.groovy
+++ groovy/l00.exit.groovy
@@ -1,30 +1,31 @@
 #!/usr/bin/env groovy
-import sys
-
 // illustrative variables
-ARG_COUNT   = len(sys.argv) - 1 // get num of real arguments
-SCRIPT_NAME = sys.argv[0]       // get name of script
-EX_USAGE    = 64;               // status for command line usage error
-EX_OK       = 0;                // status for successful termination
+ARG_COUNT   = args.size() // get num of real arguments
+SCRIPT_NAME = getClass().protectionDomain.codeSource.location.path.split('/')[-1]  
+EX_USAGE    = 64;         // status for command line usage error
+EX_OK       = 0;          // status for successful termination
 
 // method (subroutine) to output usage message to stderr
-def usage_message ():
-   sys.stderr.write("\nYou need to enter two or more numbers: \n\n")
-   sys.stderr.write("   Usage: %s [num1] [num2] [num3]...\n\n" % SCRIPT_NAME)
-   sys.exit(EX_USAGE)
+def usageMessage() {
+   System.err.println("\nYou need to enter two numbers: \n\n")
+   System.err.println("   Usage: $SCRIPT_NAME [num1] [num2]\n\n")
+   System.exit(EX_USAGE)
+}
 
 // method (subroutine) to sum numbers from array
-def addNums (numbers):
-   sum = 0                      // initialize starting sum value
-   for num in numbers:          // collection loop toprocess numbers
-     sum += int(num)            // sum up nums
-   print "The summation is: %d" % sum // output results
-   sys.exit(EX_OK)
+def addNums (Object[] numbers) {
+   sum = 0                          // initialize starting sum value
+   numbers.each { num ->            // collection loop toprocess numbers
+     sum += num.toInteger()         // sum up nums
+   }  
+   println "The summation is: $sum" // output results
+   System.exit(EX_OK)
+}
 
 // check for only 2 arguments
-if ARG_COUNT < 1:
+if (ARG_COUNT < 1)
    // ouptut usage statement to standard error
-   usage_message()
-else:
+   usageMessage()
+else
    // call addNums to process arguments
-   addNums(sys.argv[1:])
+   addNums(args)
