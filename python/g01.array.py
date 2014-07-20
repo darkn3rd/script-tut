c41f4b0a951b29285b532371f82b9b4e9826db76
지훈 <sauvecali@yahoo.com>
2014-07-19 20:46:00 -0700

    python array update

---
+++ python/g01.array.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+nicknames = []           # create an empty list
+
+# populate list one item at a time 
+nicknames.append("bob")
+nicknames.append("ed")
+nicknames.append("steve")
+nicknames.append("ralph")
+nicknames.append("joe")
+nicknames.append("deb")
+nicknames.append("kate")
+
+# output length and all values 
+print "The total nicknames are: ", len(nicknames)
+print "The nicknames are: ", nicknames
