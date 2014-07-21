8a0653ef6abbbeccb1f02045b09ceb0e36b88a7f
지훈 <sauvecali@yahoo.com>
2014-07-20 18:34:09 -0700

    annoying

--- groovy/f00.loop.groovy
+++ groovy/f00.loop.groovy
@@ -2,17 +2,8 @@
 // use collection loop on directory listing
 //  item represents the file name
 for (item in new File('.').list()) { 
-<<<<<<< HEAD
     if (new File(item).isDirectory())
         println "$item is a directory"
     else
         println "$item is a not a directory"
-
 }
-=======
-	if (new File(item).isDirectory())
-		println "$item is a directory"
-	else
-	    println "$item is a not a directory"
-}
->>>>>>> dfb804d74ee43c00020d8c86c8c809cff19bd565
