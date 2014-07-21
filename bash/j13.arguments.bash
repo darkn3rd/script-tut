cb529a77a9da41a712f7bce5cca01eeab5eb794d
지훈 <sauvecali@yahoo.com>
2014-07-21 00:15:19 -0700

    bash args update

--- bash/j13.arguments.bash
+++ bash/j13.arguments.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 # illustrative variables
-min_count=0  # set mininum number of arguments
+MIN_COUNT=0  # set mininum number of arguments
 # utility variables
 count=1      # initialize counter
  
 echo "The arugments passed are:"
 #  conditional loop with shift to enumerate args
-while (( $# > $min_count )); do # fetch updated number of arguments
+while (( $# > $MIN_COUNT )); do # fetch updated number of arguments
   arg=$1                        # fetch arg from first positional parameter
   echo " item $count: $arg"     # output count and arg
   count=$(( $count + 1 ))       # increment counter
