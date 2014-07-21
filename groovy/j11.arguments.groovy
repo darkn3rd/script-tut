c3a1660c95cea90f0751de3a62981eede13c7e68
지훈 <sauvecali@yahoo.com>
2014-07-20 17:43:51 -0700

    groovy argument forward update

--- groovy/j11.arguments.groovy
+++ groovy/j11.arguments.groovy
@@ -1,13 +1,4 @@
 #!/usr/bin/env groovy
-// illustrative variables 
-ARG_COUNT = args.size()   // get num of real arguments
-FIRST     = 0             // index of first argument
-LAST      = ARG_COUNT - 1 // index of last argument
-
 println "The arugments passed are:"
-// use collection loop with range to enumerate arguments
-for (count in FIRST..LAST) {    // range to gen sequence of numbers
-   arg = args[count]            // get arg using index
-   // output count and argument using count index
-   println " item ${count+1}: $arg"
-}
+// iteration with eachWithIndex to output arg and count
+args.eachWithIndex{ arg, count -> println " item ${count+1}: $arg" }
--- groovy/j12.arguments.groovy
+++ groovy/j12.arguments.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
-// illustrative variables
+// illustrative variables 
 ARG_COUNT = args.size()   // get num of real arguments
-LAST      = ARG_COUNT - 1 // index of last argument
 FIRST     = 0             // index of first argument
+LAST      = ARG_COUNT - 1 // index of last argument
 
 println "The arugments passed are:"
-// use count style loop
-for (count = FIRST; count <= LAST; count ++) {
-   arg = args[count]      // get arg using index
+// use collection loop with range to enumerate arguments
+for (count in FIRST..LAST) {    // range to gen sequence of numbers
+   arg = args[count]            // get arg using index
    // output count and argument using count index
    println " item ${count+1}: $arg"
 }
