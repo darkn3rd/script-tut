ccb56974527d76bb16b5ab471b0f47d71f8744f3
darkn3rd <suavecali@yahoo.com>
2014-08-08 16:46:44 -0700

    powershell function update

--- powershell/m20.function.ps1
+++ powershell/m20.function.ps1
@@ -2,17 +2,17 @@
 
 # create the function
 Function Sort-Array ($array) {
-    $array | Sort-Object
+    $array | Sort-Object # return list
 }
 
 # initialize initial array (list)
 $array = "bob", "ed", "steve", "ralph", "joe", "deb", "kate"
 
 # output current list before calling function
- "Current names are: $array" 
+"Current names are: $array" 
 
 # call the function
-result = Sort-Array $array
+$result = Sort-Array $array
 
 # output the result
 "Sorted names are:  $result"
