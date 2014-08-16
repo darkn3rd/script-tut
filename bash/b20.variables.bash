ef6a5b0ac365d6af63e0c2178a0ce6a333d2035c
지훈 <sauvecali@yahoo.com>
2014-08-15 22:36:18 -0700

    bash variables

--- bash/b20.variables.bash
+++ bash/b20.variables.bash
@@ -4,7 +4,7 @@ declare -i num=10/2        # explicitly set an integer
 char=a                     # set a character
 string="This is a string"  # set a string
 
-# show the results
-printf "Number is $num.\n" $num
-printf "Character is \'$char\'.\n" $char
-printf "String is \"$string\".\n" $string
+# show the results using formatting
+printf "Number is %d.\n" $num
+printf "Character is \'%c\'.\n" $char
+printf "String is \"%s\".\n" $string
