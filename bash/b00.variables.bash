06dfc15487a0b983cee827789c7e10e53bb2d84e
지훈 <sauvecali@yahoo.com>
2014-08-15 22:18:50 -0700

    bash variable update

--- bash/b00.variables.bash
+++ bash/b00.variables.bash
@@ -4,7 +4,7 @@ declare -i num=10/2        # explicitly set an integer
 char=a                     # set a character
 string="This is a string"  # set a string
 
-# show the results
-printf "Number is $num.\n" $num
-printf "Character is \'$char\'.\n" $char
-printf "String is \"$string\".\n" $string
+# show the results using string concatenation
+echo Number is $num.
+echo Character is \'$char\'.
+echo String is \"$string\".
--- bash/b10.variables.bash
+++ bash/b10.variables.bash
@@ -4,7 +4,7 @@ declare -i num=10/2        # explicitly set an integer
 char=a                     # set a character
 string="This is a string"  # set a string
 
-# show the results
-echo Number is $num.
-echo Character is \'$char\'.
-echo String is \"$string\".
+# show the results using interpolation
+echo "Number is $num."
+echo "Character is '$char'."
+echo "String is \"$string\"."
