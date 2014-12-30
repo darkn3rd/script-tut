9a515275874c522f307067cea67143d720935b4b
Joaquín Menchaca <jmenchaca@verticalresponse.com>
2014-12-30 10:18:19 -0800

    ScriptBox ConfigBox Puppet

--- configbox/puppet/modules/scriptbox/manifests/init.pp
+++ configbox/puppet/modules/scriptbox/manifests/init.pp
@@ -1,6 +1,24 @@
 class scriptbox (
-  $blah = $scriptbox::params::blah,
+  $shell_posix   = $scriptbox::params::shell_posix,
+  $shell_bash    = $scriptbox::params::shell_bash,
+  $shell_ksh     = $scriptbox::params::shell_ksh,
+  $shell_csh     = $scriptbox::params::shell_csh,
+  $script_awk    = $scriptbox::params::script_awk,
+  $script_groovy = $scriptbox::params::script_groovy",
+  $script_perl   = $scriptbox::params::script_perl,
+  $script_php    = $scriptbox::params::script_php,
+  $script_python = $scriptbox::params::script_python,
+  $script_ruby   = $scriptbox::params::script_ruby",
+  $script_tcl    = $scriptbox::params::script_tcl,
 ) inherits scriptbox::params {
+
+  $shells = [$shell_posix, $shell_bash, $shell_ksh, $shell_csh]
+  $scripting = [$script_awk, $script_groovy, $script_perl, $script_php,
+                $script_python, $script_ruby, $script_tcl]
+
+  package { $shells:    ensure => latest }
+  package { $scripting: ensure => latest }
+
 
 
 }
--- configbox/puppet/modules/scriptbox/manifests/params.pp
+++ configbox/puppet/modules/scriptbox/manifests/params.pp
@@ -1,3 +1,19 @@
-class jenkins::params {
-  $blah = "value"
+class scriptbox::params {
+  case $operatingsystem {
+    ubuntu: {
+      $shell_posix   = "dash",
+      $shell_bash    = "bash",
+      $shell_ksh     = "ksh",
+      $shell_csh     = "tcsh",
+      $script_awk    = "gawk",
+      $script_groovy = "groovy",
+      $script_perl   = "perl",
+      $script_python = "python",
+      $script_php    = "php5-cli",
+      $script_ruby   = "ruby",
+      $script_tcl    = "tcl8.5",
+    }
+    default: { fail("Unrecognized operating system for scriptbox") }
+  }
+
 }
