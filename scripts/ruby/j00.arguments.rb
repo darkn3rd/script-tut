efd60990f8a71207ea72ba3b573d1af26e5089bc
지훈 <sauvecali@yahoo.com>
2015-01-08 23:44:44 -0800

    Ruby: fixed trying white spaces in output

--- scripts/ruby/j00.arguments.rb
+++ scripts/ruby/j00.arguments.rb
@@ -6,11 +6,11 @@ script_name = $0          # get script name
 # check for only 2 arguments
 if arg_count != 2 then
    # output usage statement to standard error
-   $stderr.puts "\nYou need to enter two numbers: \n\n"
-   $stderr.puts "   Usage: #{script_name} [num1] [num2]\n\n" 
+   $stderr.puts "\nYou need to enter two numbers:\n\n"
+   $stderr.puts "   Usage: #{script_name} [num1] [num2]\n\n"
 else
    # get sum of both arguments, force cast them to integer
    sum = ARGV[0].to_i + ARGV[1].to_i
    # print results of both arguments (string) and summation (integer)
-   puts "The sum of #{ARGV[0]} and #{ARGV[1]} is: #{sum}"
+   puts "The sum of #{ARGV[0]} and #{ARGV[1]} is: #{sum}."
 end
