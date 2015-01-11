ededa670ed9ac369c1d185aa44499ae951c1ec6d
지훈 <sauvecali@yahoo.com>
2015-01-11 10:28:23 -0800

    Ruby: Refactor 2 count loops

--- gen_scripts/ruby/f12.loop.rb
+++ gen_scripts/ruby/f12.loop.rb
@@ -2,6 +2,6 @@
 # count style loop using range downto iterator
 #  to generate sequence of numbers that is fed
 #  into collection loop
-for count in (10).downto(0) do
+for count in (10).downto(1) do
   puts "Count is #{count}"
 end
--- gen_scripts/ruby/f13.loop.rb
+++ gen_scripts/ruby/f13.loop.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-# counte style loop using downto iterator to 
+# counte style loop using downto iterator to
 #  generate sequence of numbers
-10.downto(0) do
+10.downto(1) do
   | count |
   puts "Count is #{count}"
 end
