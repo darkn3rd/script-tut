18bfec01f02879e45e131d3f6eb91e7104affe15
지훈 <sauvecali@yahoo.com>
2015-01-11 12:37:56 -0800

    Ruby: correct output in loop tutorials

--- gen_scripts/ruby/f30.loop.rb
+++ gen_scripts/ruby/f30.loop.rb
@@ -2,10 +2,10 @@
 # pure spin loop using continous loop construct
 #   break is used to exit loop
 loop do
-  print "Enter you name (quit to Exit): " # print prompt
+  print "Enter your name (quit to Exit): " # print prompt
   STDOUT.flush                            # flush buffer to show prompt
   answer = gets.chomp                     # get string input
- 
+
   if answer.chomp == "quit"               # check for exit
       break                               # exit loop
   end
--- gen_scripts/ruby/f40.loop.rb
+++ gen_scripts/ruby/f40.loop.rb
@@ -2,14 +2,14 @@
 # pure spin loop using continous loop construct
 #   break is used to exit loop, next to skip loop
 loop do
-  print "Enter you name (quit to Exit): " # print prompt
+  print "Enter your name (quit to Exit): " # print prompt
   STDOUT.flush                            # flush buffer to show prompt
   answer = gets.chomp                     # get string input
- 
+
   if answer =~ /^[\s\t]*$/
     next                                  # skip loop if not answer entered
   end
-  
+
   if answer.chomp == "quit"
     break                                 # exit loop if user wants to quit
   end
