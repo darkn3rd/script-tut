22ae1b758462e674931057cc8ed6ba2f0e44eab3
지훈 <sauvecali@yahoo.com>
2015-01-11 10:35:48 -0800

    PHP: Refactor collection loop to workaround PHP auto-inserting newlines into stream

--- gen_scripts/php/f00.loop.php
+++ gen_scripts/php/f00.loop.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 // collection loop on results
-foreach (preg_split("/\s+/", shell_exec('ls dirtest')) as $item) {
+foreach (preg_split("/\s+/", rtrim(shell_exec('ls dirtest'))) as $item) {
    if (is_dir("dirtest/$item")) {
        echo "$item is a directory\n";
    } else {
