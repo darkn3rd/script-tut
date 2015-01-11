667401a337a51b6786a2a6327ccaa491184e28b8
지훈 <sauvecali@yahoo.com>
2015-01-11 10:25:37 -0800

    Groovy: fixed collection loops to conditionally skip past lines that have total

--- gen_scripts/groovy/f02.loop.groovy
+++ gen_scripts/groovy/f02.loop.groovy
@@ -3,13 +3,14 @@
 // collection loop where each line is fed into collection
 //   line represents a string from the shell execution output
 for (line in "ls -l dirtest".execute().text.split("\n")) {
-    if (line =~ /^total/) continue
-    // extract permissions and filename columns
-    (perms,item) = line.split()[0,-1]
+    if ( ! (line =~ /^total/)) {
+      // extract permissions and filename columns
+      (perms,item) = line.split()[0,-1]
 
-    // determine is permissions begins with a 'd' to indicate directory
-    if (perms =~ /^d/)
-        println "${item} is a directory"
-    else
-        println "${item} is not a directory"
+      // determine is permissions begins with a 'd' to indicate directory
+      if (perms =~ /^d/)
+          println "${item} is a directory"
+      else
+          println "${item} is not a directory"
+    }
 }
--- gen_scripts/groovy/f03.loop.groovy
+++ gen_scripts/groovy/f03.loop.groovy
@@ -2,12 +2,14 @@
 // iterate through each item in current directory
 //   line represents a string from the shell execution output
 "ls -l dirtest".execute().text.eachLine { line ->
+  if ( ! (line =~ /^total/)) {
     // extract permissions and filename columns
     (perms,item) = line.split()[0,-1]
 
     // determine is permissions begins with a 'd' to indicate directory
     if (perms =~ /^d/)
         println "${item} is a directory"
-    else if (perms =~ /^-/)
+    else
         println "${item} is not a directory"
+  }
 }
