f9dbb3c51fc37a95cf953e75cc6fd6a65427cccb
지훈 <sauvecali@yahoo.com>
2015-06-15 01:43:22 -0700

    Vagrant/Puppet/CentOS update

--- configbox/puppet/manifests/site.pp
+++ configbox/puppet/manifests/site.pp
@@ -1,11 +1,19 @@
 node mybox { include scriptbox }
 
 
-# Very Basic Puppet Functionality
+# Puppet Cave-Man Config style
+# ===================================
+# These systems are named after their system. This is not ideal, but it is
+# easiest to understand.
+#
+# Notes:
+#  - Best Practices would be to put these in a module, and have a module
+#  dynamically configure itself regardless of system.
+# ===================================
 
 node wheezybox {
-  $shells = ["dash", "bash", "ksh", "tcsh"]
-  $scripting = ["gawk", "perl", "python", "php5-cli", "ruby", "tcl8.5"]
+  $shells   = ["dash", "bash", "ksh", "tcsh"]
+  $script   = ["gawk", "perl", "python", "php5-cli", "ruby", "tcl8.5"]
   $packages = [$shells, $scripting]
 
   package { $packages: ensure => "installed"}
@@ -15,9 +23,17 @@ node wheezybox {
 }
 
 node centos6box {
-  $shells = ["dash", "bash", "ksh", "tcsh"]
-  $scripting = ["gawk", "perl", "python", "php-cli", "ruby", "tcl"]
+  $shells   = ["dash", "bash", "ksh", "tcsh"]
+  $script   = ["gawk", "perl", "python", "php-cli", "ruby", "tcl"]
   $packages = [$shells, $scripting]
 
   package { $packages: ensure => "installed"}
+
+  # Install OpenJDK for Rhino, Groovy, Etc.
+  package { "java-1.8.0-openjdk-devel": ensure => "installed"}
+
+  # GVM is complicated, and running gvmtool doesn't work in puppet-apply
+  # https://github.com/jenkins-infra/puppet-groovy
+  # https://github.com/paulosuzart/gvm
+
 }
