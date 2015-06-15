0963cdf13b9959e4389330cb2f6d5c4759205542
지훈 <sauvecali@yahoo.com>
2015-06-15 00:39:28 -0700

    Vagrant Update

--- configbox/puppet/manifests/site.pp
+++ configbox/puppet/manifests/site.pp
@@ -1,1 +1,23 @@
 node mybox { include scriptbox }
+
+
+# Very Basic Puppet Functionality
+
+node wheezybox {
+  $shells = ["dash", "bash", "ksh", "tcsh"]
+  $scripting = ["gawk", "perl", "python", "php5-cli", "ruby", "tcl8.5"]
+  $packages = [$shells, $scripting]
+
+  package { $packages: ensure => "installed"}
+
+
+  #$script_groovy = "groovy"
+}
+
+node centos6box {
+  $shells = ["dash", "bash", "ksh", "tcsh"]
+  $scripting = ["gawk", "perl", "python", "php-cli", "ruby", "tcl"]
+  $packages = [$shells, $scripting]
+
+  package { $packages: ensure => "installed"}
+}
