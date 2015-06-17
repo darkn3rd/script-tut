e32bece4d6e7c584912ed17702ff8e57e48bf4f5
Joaquin Menchaca <joaquin@fandor.com>
2015-06-17 15:20:00 -0700

    Configbox/Chef

--- configbox/chef/site-cookbooks/centos6box/recipes/default.rb
+++ configbox/chef/site-cookbooks/centos6box/recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+shells   = %w{dash bash ksh tcsh}
+script   = %w{gawk perl python php-cli ruby tcl}
+packages = [shells, script]
+
+packages.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
--- configbox/chef/site-cookbooks/wheezybox/recipes/default.rb
+++ configbox/chef/site-cookbooks/wheezybox/recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+shells   = %w{dash bash ksh tcsh}
+script   = %w{gawk perl python php5-cli ruby tcl8.5}
+packages = [shells, script]
+
+packages.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
