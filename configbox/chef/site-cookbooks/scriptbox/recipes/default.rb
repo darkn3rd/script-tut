7cb45f058aa188dbf8eb6cd14340235133d320c6
Joaquin Menchaca <joaquin@fandor.com>
2015-06-18 11:03:54 -0700

    chef

--- configbox/chef/site-cookbooks/scriptbox/recipes/default.rb
+++ configbox/chef/site-cookbooks/scriptbox/recipes/default.rb
@@ -10,10 +10,12 @@
 packages = ""
 
 if node[:platform_family].include?("rhel")
+  # Package names tested on CentOS 6.6
   shells   = %w{dash bash ksh tcsh}
   script   = %w{gawk perl python php-cli ruby tcl}
   packages = [shells, script]
 elsif node[:platform_family].include?("debian")
+  # Package names tested on Debian 7.8
   shells   = %w{dash bash ksh tcsh}
   script   = %w{gawk perl python php5-cli ruby tcl8.5}
   packages = [shells, script]
